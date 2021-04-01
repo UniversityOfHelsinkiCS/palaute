1439996148d94a8c43465c463194c40a74d131f7
Jami Kousa <jami.kousa@helsinki.fi>
2021-04-01 14:02:41 +0300

    add GIT_SHA info for sentry

--- src/server/util/sentry.js
+++ src/server/util/sentry.js
@@ -1,19 +1,15 @@
 const Sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing') // eslint-disable-line
 // Sentry docs Note: You MUST import the package for tracing to work
-const { inProduction } = require('./config')
+const { inProduction, GIT_SHA } = require('./config')
 
 const initializeSentry = () => {
   if (!inProduction) return
 
   Sentry.init({
     dsn: 'https://8877ea30aa714216b27b22c8aa395723@sentry.cs.helsinki.fi/6',
-    integrations: [
-      // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-    ],
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
+    release: GIT_SHA,
+    integrations: [new Sentry.Integrations.Http({ tracing: true })],
     tracesSampleRate: 1.0,
   })
 }
