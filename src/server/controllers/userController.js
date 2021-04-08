5fed849e59627c1f25f31e2dec29a67f83eeda21
LeoVaris <leovaris04@gmail.com>
2021-04-08 21:50:52 +0300

    remove logs

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -2,7 +2,7 @@ const { ApplicationError } = require('../util/customErrors')
 
 const getUser = async (req, res) => {
   const { user } = req
-  console.log('user', user)
+
   if (!user) throw new ApplicationError('Not found', 404)
 
   res.send(user)
