37f309bd805663bd47d06023965c8a38bdd71f33
LeoVaris <leovaris04@gmail.com>
2021-04-12 14:06:52 +0300

    mangle enrolments

--- src/server/controllers/enrolmentController.js
+++ src/server/controllers/enrolmentController.js
@@ -1,23 +1,23 @@
-// const dateFns = require('date-fns')
+const dateFns = require('date-fns')
 const { ApplicationError } = require('../util/customErrors')
 
-// onst { getEnrolmentByPersonId } = require('../util/importerEnrolled')
+const { getEnrolmentByPersonId } = require('../util/importerEnrolled')
 
 const getEnrolmentsByUser = async (req, res) => {
   const { user } = req
 
   if (!user) throw new ApplicationError('Missing uid header', 403)
-  // const { id } = user
+  const { id } = user
 
-  // const startDateBefore = dateFns.subDays(new Date(), 14)
-  // const endDateAfter = dateFns.subDays(new Date(), 14)
+  const startDateBefore = dateFns.subDays(new Date(), 14)
+  const endDateAfter = dateFns.subDays(new Date(), 14)
 
-  /* const enrolments = await getEnrolmentByPersonId(id, {
+  const enrolments = await getEnrolmentByPersonId(id, {
     startDateBefore,
     endDateAfter,
-  }) */
+  })
 
-  res.send([])
+  res.send(enrolments)
 }
 
 module.exports = {
--- src/server/util/importerEnrolled.js
+++ src/server/util/importerEnrolled.js
@@ -2,28 +2,111 @@ const dateFns = require('date-fns')
 
 const importerClient = require('./importerClient')
 
-const { FeedbackTarget } = require('../models')
-const { Question } = require('../models')
-
-const defaultQuestions = require('./questions.json')
+const { FeedbackTarget, CourseUnit } = require('../models')
 
 const formatDate = (date) => dateFns.format(date, 'yyyy-MM-dd')
 
-const createFeedbackTarget = async (data) => {
-  const [course] = await FeedbackTarget.upsert({
+const createCourseUnit = async (data) => {
+  await CourseUnit.upsert({
     id: data.id,
-    endDate: data.activityPeriod.endDate,
+    name: data.name,
   })
-  await Question.findOrCreate({
-    where: {
-      courseRealisationId: data.id,
-    },
-    defaults: {
-      courseRealisationId: data.id,
-      data: defaultQuestions,
-    },
+}
+
+const createFeedbackTargetFromAssessmentItem = async (
+  data,
+  endDate,
+  courseUnit,
+) => {
+  await createCourseUnit(courseUnit)
+  const [course] = await FeedbackTarget.upsert({
+    feedbackType: 'assessmentItem',
+    typeId: data.id,
+    courseUnitId: courseUnit.id,
+    name: data.name,
+    opensAt: formatDate(dateFns.subDays(endDate, 14)),
+    closesAt: formatDate(dateFns.addDays(endDate, 14)),
   })
   return course
+}
+
+const createFeedbackTargetFromStudyGroup = async (
+  data,
+  endDate,
+  courseUnitId,
+) => {
+  const [target] = await FeedbackTarget.upsert({
+    feedbackType: 'studySubGroup',
+    typeId: data.id,
+    courseUnitId,
+    name: data.name,
+    opensAt: formatDate(dateFns.subDays(endDate, 14)),
+    closesAt: formatDate(dateFns.addDays(endDate, 14)),
+  })
+  return target
+}
+
+const createFeedbackTargetFromCourseRealisation = async (
+  data,
+  studySubGroupIds,
+  courseUnitId,
+) => {
+  const endDate = dateFns.parse(
+    data.activityPeriod.endDate,
+    'yyyy-MM-dd',
+    new Date(),
+  )
+  const studySubGroupItems = (
+    await Promise.all(
+      data.studyGroupSets.map(async (studySet) => {
+        const studySetItems = await Promise.all(
+          studySet.studySubGroups
+            .filter((group) => studySubGroupIds.includes(group.id))
+            .map(async (item) =>
+              createFeedbackTargetFromStudyGroup(item, endDate, courseUnitId),
+            ),
+        )
+        return studySetItems
+      }),
+    )
+  ).flat()
+  const [course] = await FeedbackTarget.upsert({
+    feedbackType: 'courseRealisation',
+    typeId: data.id,
+    courseUnitId,
+    name: data.name,
+    opensAt: formatDate(dateFns.subDays(endDate, 14)),
+    closesAt: formatDate(dateFns.addDays(endDate, 14)),
+  })
+  studySubGroupItems.push(course)
+  return studySubGroupItems
+}
+
+const createTargetsFromEnrolment = async (data) => {
+  const studySubGroupIds = data.studySubGroups.map(
+    (group) => group.studySubGroupId,
+  )
+
+  const { courseUnitId, courseUnitRealisation, courseUnit } = data
+  const endDate = dateFns.parse(
+    courseUnitRealisation.activityPeriod.endDate,
+    'yyyy-MM-dd',
+    new Date(),
+  )
+  await createCourseUnit(data.courseUnit)
+  const assessmentItem = await createFeedbackTargetFromAssessmentItem(
+    data.assessmentItem,
+    endDate,
+    courseUnit,
+  )
+
+  const realisationItems = await createFeedbackTargetFromCourseRealisation(
+    courseUnitRealisation,
+    studySubGroupIds,
+    courseUnitId,
+  )
+  realisationItems.push(assessmentItem)
+  return realisationItems
 }
 
 const getEnrolmentByPersonId = async (personId, options = {}) => {
@@ -37,12 +120,11 @@ const getEnrolmentByPersonId = async (personId, options = {}) => {
   const { data } = await importerClient.get(`/palaute/enrolled/${personId}`, {
     params,
   })
-
-  return Promise.all(
-    data.map(async (enrollment) =>
-      createFeedbackTarget(enrollment.course_unit_realisation),
-    ),
-  )
+  return (
+    await Promise.all(
+      data.map(async (enrolment) => createTargetsFromEnrolment(enrolment)),
+    )
+  ).flat()
 }
 
 module.exports = {
