a9fe74538c288e57a952f6dc12486ea434cc33cb
Jami Kousa <jami.kousa@helsinki.fi>
2021-04-19 16:07:48 +0300

    fix default value

--- src/server/migrations/20210419_03_add_question_ids_default_to_surveys.js
+++ src/server/migrations/20210419_03_add_question_ids_default_to_surveys.js
@@ -2,12 +2,12 @@ const { ARRAY, INTEGER } = require('sequelize')
 
 module.exports = {
   up: async (queryInterface) => {
+    await queryInterface.removeColumn('surveys', 'question_ids')
     await queryInterface.addColumn('surveys', 'question_ids', {
       type: ARRAY(INTEGER),
       allowNull: false,
+      defaultValue: [],
     })
   },
-  down: async (queryInterface) => {
-    await queryInterface.removeColumn('surveys', 'question_ids')
-  },
+  down: async () => {},
 }
