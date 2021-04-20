656db14113c23a06b8aee096e4c46f5b9aca4e29
Jami Kousa <jami.kousa@helsinki.fi>
2021-04-20 15:22:56 +0300

    remove course_realisation id from questions

---
+++ src/server/migrations/20210420_02_remove_course_realisation_id_from_questions.js
@@ -0,0 +1,12 @@
+const { INTEGER } = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.removeColumn('questions', 'course_realisation_id')
+  },
+  down: async (queryInterface) => {
+    await queryInterface.addColumn('questions', 'course_realisation_id', {
+      type: INTEGER,
+    })
+  },
+}
