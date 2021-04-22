6289f5f4e8ade056be52e8717b7fffa8102be2f7
Olli Keski-Hynnilä <eragah@gmail.com>
2021-04-22 13:32:20 +0300

    exams no longer generate feedback targets + refactoring importer mangels

---
+++ src/server/util/importerHelpers.js
@@ -0,0 +1,75 @@
+const dateFns = require('date-fns')
+
+const {
+  makeCreateFeedbackTargetWithUserTargetTable,
+  combineStudyGroupName,
+} = require('./importerCommon')
+
+const createFeedbackTargetFromCourseRealisation = async (
+  realisation,
+  userId,
+  courseUnit,
+  studySubGroupIds,
+) => {
+  const personType = studySubGroupIds ? 'STUDENT' : 'TEACHER'
+
+  const createFeedbackTargetWithUserTargetTable = makeCreateFeedbackTargetWithUserTargetTable(
+    personType,
+  )
+
+  if (
+    realisation.courseUnitRealisationTypeUrn ===
+    'urn:code:course-unit-realisation-type:exam-exam'
+  )
+    return
+
+  const { id: courseUnitId } = courseUnit
+
+  const endDate = dateFns.parse(
+    realisation.activityPeriod.endDate,
+    'yyyy-MM-dd',
+    new Date(),
+  )
+
+  const startDate = dateFns.parse(
+    realisation.activityPeriod.startDate,
+    'yyyy-MM-dd',
+    new Date(),
+  )
+
+  await createFeedbackTargetWithUserTargetTable(
+    'courseRealisation',
+    realisation.id,
+    realisation.id,
+    courseUnitId,
+    realisation.name,
+    endDate,
+    startDate,
+    userId,
+  )
+  await realisation.studyGroupSets.reduce(async (prom, studySet) => {
+    await prom
+    const setName = studySet.name
+    await studySet.studySubGroups
+      .filter((group) =>
+        studySubGroupIds ? studySubGroupIds.includes(group.id) : true,
+      )
+      .reduce(async (promise, group) => {
+        await promise
+        await createFeedbackTargetWithUserTargetTable(
+          'studySubGroup',
+          group.id,
+          realisation.id,
+          courseUnitId,
+          combineStudyGroupName(setName, group.name),
+          endDate,
+          startDate,
+          userId,
+        )
+      }, Promise.resolve())
+  }, Promise.resolve())
+}
+
+module.exports = {
+  createFeedbackTargetFromCourseRealisation,
+}
