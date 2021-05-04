a37e84435c62b3871f9123122ee399eba3804819
Jami Kousa <jami.kousa@helsinki.fi>
2021-05-04 16:30:52 +0300

    win has won?

--- scripts/get_prod_db.sh
+++ scripts/get_prod_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CONTAINER=palaute_db
 SERVICE_NAME=db
 DB_NAME=postgres
 
@@ -56,7 +57,7 @@ docker-compose -f $DOCKER_COMPOSE down -v
 echo "Starting postgres in the background"
 docker-compose -f $DOCKER_COMPOSE up -d $SERVICE_NAME
 
-retry docker-compose -f $DOCKER_COMPOSE exec $SERVICE_NAME pg_isready --dbname=$DB
+retry docker-compose -f $DOCKER_COMPOSE exec $SERVICE_NAME pg_isready
 
-echo "Populating $DB"
-docker-compose -f $DOCKER_COMPOSE exec -T $SERVICE_NAME /bin/bash -c "gunzip | psql -U postgres" < ${BACKUPS}${SERVER_FILE_NAME}
+echo "Populating"
+docker exec -i $CONTAINER /bin/bash -c "gunzip | psql -U postgres" < ${BACKUPS}${SERVER_FILE_NAME}
