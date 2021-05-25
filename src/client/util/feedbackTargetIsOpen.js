a063a271c2b8a778572c523eb983ac688130cd0e
Olli Keski-Hynnilä <eragah@gmail.com>
2021-05-25 16:35:59 +0300

    kludge for feedback targets

--- src/client/util/feedbackTargetIsOpen.js
+++ src/client/util/feedbackTargetIsOpen.js
@@ -1,7 +1,11 @@
+import { parseISO, getYear } from 'date-fns'
+
 const feedbackTargetIsOpen = (feedbackTarget) => {
   if (!feedbackTarget?.opensAt || !feedbackTarget?.closesAt) {
     return true
   }
+
+  if (getYear(parseISO(feedbackTarget.opensAt)) === 2019) return false
 
   const { opensAt, closesAt } = feedbackTarget
   const now = new Date()
