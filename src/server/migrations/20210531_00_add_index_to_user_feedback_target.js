4be6dad6c93c2ade88d36bf15b87bc67d6d2fcfe
LeoVaris <leovaris04@gmail.com>
2021-05-31 12:22:51 +0300

    add index

---
+++ src/server/migrations/20210531_00_add_index_to_user_feedback_target.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.addIndex('user_feedback_targets', {
+      fields: ['user_id', 'access_status'],
+      name: 'user_feedback_target_status',
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.removeIndex('user_feedback_target_status')
+  },
+}
