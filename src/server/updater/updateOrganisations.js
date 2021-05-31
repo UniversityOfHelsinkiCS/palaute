7efc5686744f85b3f7b9f41e839475545ff236b6
Leo Varis <leovaris04@gmail.com>
2021-05-31 21:18:26 +0300

    fix

--- src/server/updater/updateOrganisations.js
+++ src/server/updater/updateOrganisations.js
@@ -2,7 +2,15 @@ const { Organisation } = require('../models')
 const mangleData = require('./updateLooper')
 
 const organisationsHandler = async (organisations) => {
-  await Organisation.bulkCreate(organisations, {
+  const ids = new Set() // TODO fix
+  const uniqueOrganisations = organisations.filter((org) => {
+    if (!ids.has(org.id)) {
+      ids.add(org.id)
+      return true
+    }
+    return false
+  })
+  await Organisation.bulkCreate(uniqueOrganisations, {
     updateOnDuplicate: ['name', 'code', 'parentId'],
   })
 }
