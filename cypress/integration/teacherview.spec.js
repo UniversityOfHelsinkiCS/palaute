aabb6faa4160e3a70150d7da5260787848b58e32
Olli Keski-Hynnilä <eragah@gmail.com>
2021-06-17 12:07:44 +0300

    fixed tests

--- cypress/integration/teacherview.spec.js
+++ cypress/integration/teacherview.spec.js
@@ -6,15 +6,13 @@ describe('Teacher view', function () {
     cy.contains('My teaching')
     cy.contains('Ongoing courses (0)')
     cy.contains('Upcoming courses (0)')
-    cy.contains('Ended courses (6)')
+    cy.contains('Ended courses (1)')
   })
   it('A logged in teacher can view its ended courses', function () {
     cy.contains('My teaching')
     cy.contains('TKT20002 Software Development Methods')
     cy.get('div').contains('TKT20002 Software Development Methods').click()
     cy.contains('16.03.2021 - 08.05.2021')
-    cy.get('div').contains('TKT20006 Software Engineering').click()
-    cy.contains('No course realisations')
   })
   it('If feedback response has not been given teacher is notified', function () {
     cy.get('p')
