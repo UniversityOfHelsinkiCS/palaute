7d00ef05d67a16fbddf1d3b3f40ea610cfee35d5
Kalle Ilves <kalleilv@gmail.com>
2021-06-28 14:41:07 +0300

    disable all courses

---
+++ src/server/migrations/20210628_01_update_disabled_course_codes.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+    UPDATE organisations
+    SET disabled_course_codes = ARRAY(
+    SELECT DISTINCT ON (course_units.course_code)
+    course_units.course_code
+    FROM course_units
+    INNER JOIN course_units_organisations ON course_units.id = course_units_organisations.course_unit_id
+    WHERE course_units_organisations.organisation_id = organisations.id);
+    `);
+  },
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+    UPDATE organisations
+    SET disabled_course_codes = '{}';
+    `);
+  },
+};
