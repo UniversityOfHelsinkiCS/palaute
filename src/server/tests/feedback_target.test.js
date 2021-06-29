78a6456e13fe7e6880877ee2757b2b4e3ad8f3bd
Olli Keski-Hynnilä <eragah@gmail.com>
2021-06-29 14:49:15 +0300

    some testutils and more tests

--- src/server/tests/feedback_target.test.js
+++ src/server/tests/feedback_target.test.js
@@ -1,26 +1,37 @@
 /* eslint-disable */
 const request = require('supertest')
 const app = require('../index')
-
-const headers = {
-  uid: 'mluukkai',
-  givenName: 'Matti',
-  mail: 'grp-toska+mockadmin@helsinki.fi',
-  sn: 'Luukkainen',
-  preferredLanguage: 'en',
-  hyPersonSisuId: 'hy-hlo-1441871',
-  employeeNumber: '9021313',
-}
+const { users } = require('./testutils')
 
 test('Fetching a feedback target as logged in user', async (done) => {
   const response = await request(app)
     .get('/api/feedback-targets/97')
-    .set(headers)
+    .set(users.admin)
     .expect(200)
     .expect('Content-type', /application\/json/)
 
   expect(response.body.typeId).toBe('hy-CUR-137933885')
   expect(response.body.courseUnit.courseCode).toBe('TKT20002')
+
+  done()
+})
+
+test('Feedback target can be updated by the teacher', async (done) => {
+  const newDate = new Date()
+
+  await request(app)
+    .put('/api/feedback-targets/97')
+    .send({ opensAt: newDate })
+    .set(users.admin)
+    .expect(200)
+
+  done()
+})
+
+test('Fetching a feedback target without logging in errors', async (done) => {
+  const res = await request(app).get('/api/feedback-targets/97').expect(403)
+
+  expect(res.body.error).toBe('Missing uid header')
 
   done()
 })
