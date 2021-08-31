6d4aa26d8feb85609ce6503f74497f9687a5868a
Kalle Ilves <kalleilv@gmail.com>
2021-08-31 13:42:35 +0300

    index for feedback target course realisation id

---
+++ src/server/migrations/20210831_01_add_feedback_targets_course_realisation_id_index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.addIndex('feedback_targets', {
+      fields: ['course_realisation_id'],
+      name: 'feedback_targets_course_realisation_id',
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.removeIndex('feedback_targets_course_realisation_id')
+  },
+}
