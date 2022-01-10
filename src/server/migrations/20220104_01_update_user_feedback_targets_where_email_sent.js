ba833d44b62613148b23f128e8d8e5cb66a181e5
Olli Keski-Hynnilä <eragah@gmail.com>
2022-01-10 11:28:21 +0200

    remove unused column in migration

--- src/server/migrations/20220104_01_update_user_feedback_targets_where_email_sent.js
+++ src/server/migrations/20220104_01_update_user_feedback_targets_where_email_sent.js
@@ -1,3 +1,5 @@
+const { BOOLEAN } = require('sequelize/types')
+
 module.exports = {
   up: async (queryInterface) => {
     const [id] = await queryInterface.sequelize.query(
@@ -13,18 +15,30 @@ module.exports = {
     await queryInterface.sequelize.query(
       `UPDATE user_feedback_targets SET feedback_open_email_sent = true WHERE id in (${ids})`,
     )
+
+    await queryInterface.removeColumn(
+      'feedback_targets',
+      'feedback_open_notification_email_sent',
+    )
   },
   down: async (queryInterface) => {
+    await queryInterface.addColumn(
+      'feedback_targets',
+      'feedback_open_notification_email_sent',
+      { type: BOOLEAN },
+    )
+
     const [id] = await queryInterface.sequelize.query(
-      `SELECT u.id FROM user_feedback_targets u 
-          INNER JOIN feedback_targets f on u.feedback_target_id = f.id 
-          WHERE f.feedback_open_notification_email_sent = true`,
+      `SELECT distinct(f.id) FROM feedback_targets f 
+       INNER JOIN user_feedback_targets u on f.id = u.feedback_target_id 
+       WHERE u.feedback_open_email_sent = true`,
     )
 
     const ids = id.map((i) => i.id)
+    ids.push(0)
 
     await queryInterface.sequelize.query(
-      `UPDATE user_feedback_targets SET feedback_open_email_sent = false WHERE id in (${ids})`,
+      `UPDATE feedback_targets SET feedback_open_notification_email_sent = true WHERE id in (${ids})`,
     )
   },
 }
