3e84efc5aa98320bae83df7e5dcd1f80cfa48039
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2022-03-07 12:57:16 +0200

    make datepicker typeable

--- src/client/components/FormikDatePicker.js
+++ src/client/components/FormikDatePicker.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useField } from 'formik'
 import { useTranslation } from 'react-i18next'
-import { DatePicker } from '@material-ui/pickers'
+import { KeyboardDatePicker } from '@material-ui/pickers'
 
 const FormikDatePicker = ({ name, ...props }) => {
   const [field, meta, helpers] = useField(name)
@@ -10,21 +10,28 @@ const FormikDatePicker = ({ name, ...props }) => {
   const showError = meta.error && meta.touched
 
   return (
-    <DatePicker
-      format="dd/MMM/yyyy"
-      margin="normal"
-      id={name}
-      value={field.value ?? ''}
-      onChange={(value) => {
-        helpers.setValue(value)
-      }}
-      onBlur={() => helpers.setTouched(true)}
-      error={showError}
-      helperText={showError ? t(meta.error) : ''}
-      inputVariant="outlined"
-      maxDate={new Date('2300-01-01')}
-      {...props}
-    />
+    <>
+      <KeyboardDatePicker
+        format="dd/MM/yyyy"
+        variant="inline"
+        margin="normal"
+        id={name}
+        value={field.value ?? ''}
+        onChange={(value) => {
+          helpers.setValue(value)
+        }}
+        onBlur={() => helpers.setTouched(true)}
+        error={showError}
+        helperText={showError ? t(meta.error) : ''}
+        KeyboardButtonProps={{
+          'aria-label': 'change date',
+        }}
+        inputVariant="outlined"
+        disableToolbar
+        maxDate={new Date('2300-01-01')}
+        {...props}
+      />
+    </>
   )
 }
 
