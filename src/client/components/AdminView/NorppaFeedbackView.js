1d97cea840f9fabc8f2e63d2fbe11cb64279354f
Olli Keski-Hynnilä <eragah@gmail.com>
2022-03-09 12:49:33 +0200

    admin view for norppa feedbacks

---
+++ src/client/components/AdminView/NorppaFeedbackView.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import {
+  Box,
+  Button,
+  CircularProgress,
+  makeStyles,
+  Paper,
+  Typography,
+} from '@material-ui/core'
+import { format } from 'date-fns'
+import { useSnackbar } from 'notistack'
+import { useTranslation } from 'react-i18next'
+
+import useNorppaFeedbacks from '../../hooks/useNorppaFeedbacks'
+import apiClient from '../../util/apiClient'
+
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: 10,
+  },
+  feedback: {
+    marginBottom: 5,
+    padding: 10,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  response: {
+    background: '#dde6ff',
+  },
+  text: {
+    marginBottom: 5,
+  },
+}))
+
+const NorppaFeedbackView = () => {
+  const { isLoading, feedbacks, refetch } = useNorppaFeedbacks()
+  const classes = useStyles()
+  const { enqueueSnackbar } = useSnackbar()
+  const { t } = useTranslation()
+
+  if (isLoading) {
+    return <CircularProgress />
+  }
+
+  const sortedFeedbacks = feedbacks.sort(
+    (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+  )
+
+  const handleClick = async (id) => {
+    try {
+      await apiClient.put(`/norppa-feedback/${id}`)
+      refetch()
+    } catch (e) {
+      enqueueSnackbar(t('unknownError'), { variant: 'error' })
+    }
+  }
+
+  return (
+    <Box className={classes.container}>
+      {sortedFeedbacks.map(({ id, createdAt, data, responseWanted, user }) => {
+        const created = format(new Date(createdAt), 'dd.MM.yyyy')
+        return (
+          <Paper
+            className={`${classes.feedback} ${
+              responseWanted ? classes.response : ''
+            }`}
+            key={id}
+          >
+            <Box>
+              <Typography
+                variant="body1"
+                component="p"
+                className={classes.text}
+              >
+                {data}
+              </Typography>
+              <Typography
+                variant="body2"
+                component="p"
+                className={classes.info}
+              >
+                {user.email} {created}
+              </Typography>
+            </Box>
+            <Box>
+              {responseWanted && (
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={() => handleClick(id)}
+                >
+                  Mark as solved
+                </Button>
+              )}
+            </Box>
+          </Paper>
+        )
+      })}
+    </Box>
+  )
+}
+
+export default NorppaFeedbackView
