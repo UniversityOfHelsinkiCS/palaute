03baf04fdf0b63976c22e96331c9dd15c5d80ef2
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2022-05-27 14:45:27 +0300

    Show basic organisation logs in the tab

--- src/client/components/OrganisationSettings/OrganisationLogs.js
+++ src/client/components/OrganisationSettings/OrganisationLogs.js
@@ -1,9 +1,25 @@
-import { Box } from '@material-ui/core'
+import { Box, Paper, Typography } from '@material-ui/core'
 import React from 'react'
 import { useParams } from 'react-router'
 import useAuthorizedUser from '../../hooks/useAuthorizedUser'
 import useOrganisationLogs from '../../hooks/useOrganisationLogs'
 import { LoadingProgress } from '../LoadingProgress'
+
+const getLogMessage = (data) => JSON.stringify(data)
+
+const LogItem = ({ log }) => (
+  <Box m={2}>
+    <Paper>
+      <Box display="flex" p={2}>
+        <Typography>{new Date(log.createdAt).toLocaleDateString()}</Typography>
+        <Box m={2} />
+        <Typography>{log.user.email}</Typography>
+        <Box m={2} />
+        <Typography>{getLogMessage(log.data)}</Typography>
+      </Box>
+    </Paper>
+  </Box>
+)
 
 const OrganisationLogs = () => {
   const { code } = useParams()
@@ -14,7 +30,13 @@ const OrganisationLogs = () => {
     return <LoadingProgress />
   }
 
-  return <Box>Organisation logs</Box>
+  return (
+    <Box display="flex" flexDirection="column">
+      {organisationLogs.map((log, idx) => (
+        <LogItem key={idx} log={log} />
+      ))}
+    </Box>
+  )
 }
 
 export default OrganisationLogs
