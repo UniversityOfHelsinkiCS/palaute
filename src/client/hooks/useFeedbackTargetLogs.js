100b380f7440dacfe86fd37a717ebe5659d397a0
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-05-30 10:55:35 +0300

    Create hook to fetch feedback target logs

---
+++ src/client/hooks/useFeedbackTargetLogs.js
@@ -0,0 +1,25 @@
+import { useQuery } from 'react-query'
+
+import apiClient from '../util/apiClient'
+
+const defaultCacheTime = 900000
+
+const useFeedbackTargetLogs = (id, options = {}) => {
+  const queryKey = ['feedbackTargetLogs', id]
+
+  const queryFn = async () => {
+    const { data } = await apiClient.get(`/feedback-targets/${id}/logs`)
+
+    return data
+  }
+
+  const { data: feedbackTargetLogs, ...rest } = useQuery(queryKey, queryFn, {
+    cacheTime: defaultCacheTime,
+    staleTime: defaultCacheTime,
+    ...options,
+  })
+
+  return { feedbackTargetLogs, ...rest }
+}
+
+export default useFeedbackTargetLogs
