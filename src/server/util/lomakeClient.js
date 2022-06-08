86d7a6eb6ef61e3ea8b7bd47c118776a2afb424f
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-06-08 10:41:30 +0300

    iam groups first commit

--- src/server/util/lomakeClient.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-const lomakeClient = axios.create({
-  baseURL: 'https://opetushallinto.cs.helsinki.fi/tilannekuva/api/external',
-})
-
-module.exports = lomakeClient
