ac284a373350bc1e78bb21549e574df2dfb0a295
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-07-05 12:05:31 +0300

    Manually install needed node core modules for CRA 5, configure fallbacks with react-app-rewired
    Webpack 5 no longer includes Node core modules needed by some packages. Install needed modules and override parts of CRA config"

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const webpack = require('webpack')
+
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {}
+  Object.assign(fallback, {
+    path: require.resolve('path-browserify'),
+    fs: require.resolve('browserify-fs'),
+    stream: require.resolve('stream-browserify'),
+    util: require.resolve('util/'),
+  })
+  config.resolve.fallback = fallback
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ])
+  return config
+}
