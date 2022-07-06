caaf528043c5411b1869939d36885a87078e49a9
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-07-06 14:26:36 +0300

    Move theme to mui v5

--- src/client/theme.js
+++ src/client/theme.js
@@ -1,10 +1,6 @@
 import { useMediaQuery } from '@mui/material'
-import {
-  createTheme,
-  responsiveFontSizes,
-  adaptV4Theme,
-} from '@mui/material/styles'
-import { lightBlue, green } from '@mui/material/colors'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
+import { lightBlue, green, red } from '@mui/material/colors'
 import { useMemo } from 'react'
 
 import { inStaging } from '../config'
@@ -15,25 +11,24 @@ const useTheme = () => {
   const theme = useMemo(
     () =>
       responsiveFontSizes(
-        createTheme(
-          adaptV4Theme({
-            palette: {
-              primary: {
-                main: !inStaging ? '#1077A1' : '#77dcbb',
-              },
-              info: {
-                main: lightBlue[700],
-                light: lightBlue[500],
-                dark: lightBlue[900],
-              },
-              success: {
-                main: green[800],
-                light: green[500],
-                dark: green[900],
-              },
+        createTheme({
+          palette: {
+            primary: {
+              main: !inStaging ? '#1077A1' : '#77dcbb',
             },
-          }),
-        ),
+            secondary: red,
+            info: {
+              main: lightBlue[700],
+              light: lightBlue[500],
+              dark: lightBlue[900],
+            },
+            success: {
+              main: green[800],
+              light: green[500],
+              dark: green[900],
+            },
+          },
+        }),
       ),
     [prefersDarkMode],
   )
