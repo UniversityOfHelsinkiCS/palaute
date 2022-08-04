bebc1943f5d48a504b0bd6a74ca3013f066231c5
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2022-08-04 14:56:08 +0300

    ✨Acually✨add✨untracked✨files🤦‍

---
+++ src/server/routes/testing/testingController.js
@@ -0,0 +1,122 @@
+const Router = require('express')
+
+const _ = require('lodash')
+
+const {
+  FeedbackTarget,
+  CourseRealisation,
+  Organisation,
+  User,
+} = require('../../models')
+
+const { ApplicationError } = require('../../util/customErrors')
+
+const updateCourseRealisation = async (req, res) => {
+  const { user } = req
+
+  if (!user) throw new ApplicationError('No user found', 404)
+
+  const { feedbackTargetId } = req.params
+
+  const feedbackTarget = await FeedbackTarget.findByPk(Number(feedbackTargetId))
+
+  if (!feedbackTarget)
+    throw new ApplicationError('Feedback target not found', 404)
+
+  const courseRealisation = await CourseRealisation.findByPk(
+    feedbackTarget.courseRealisationId,
+  )
+
+  if (!courseRealisation)
+    throw new ApplicationError('Course realisation not found', 404)
+
+  const updates = _.pick(req.body, ['startDate', 'endDate'])
+
+  Object.assign(courseRealisation, updates)
+
+  await courseRealisation.save()
+
+  const { feedbackResponse, feedbackResponseEmailSent } = req.body
+
+  Object.assign(feedbackTarget, {
+    opensAt: updates.startDate,
+    closesAt: updates.endDate,
+    feedbackResponse,
+    feedbackResponseEmailSent,
+  })
+
+  await feedbackTarget.save()
+
+  return res.sendStatus(200)
+}
+
+const updateManyCourseRealisations = async (req, res) => {
+  const { user } = req
+
+  if (!user) throw new ApplicationError('No user found', 404)
+
+  const { feedbackTargetIds } = _.pick(req.body, ['feedbackTargetIds'])
+
+  const updates = _.pick(req.body, ['startDate', 'endDate'])
+
+  /* eslint-disable */
+  for (const id of feedbackTargetIds) {
+    const feedbackTarget = await FeedbackTarget.findByPk(Number(id))
+
+    if (!feedbackTarget)
+      throw new ApplicationError('Feedback target not found', 404)
+
+    const courseRealisation = await CourseRealisation.findByPk(
+      feedbackTarget.courseRealisationId,
+    )
+
+    if (!courseRealisation)
+      throw new ApplicationError('Course realisation not found', 404)
+
+    Object.assign(courseRealisation, updates)
+
+    await courseRealisation.save()
+  }
+  /* eslint-enable */
+
+  return res.send(200)
+}
+
+const enableAllCourses = async (_, res) => {
+  await Organisation.update(
+    {
+      disabledCourseCodes: [],
+    },
+    { where: {} },
+  )
+  return res.send(200)
+}
+
+const updateUser = async (req, res) => {
+  const { userId } = req.params
+
+  const user = await User.findByPk(userId)
+
+  if (!user) throw new ApplicationError('User not found', 404)
+
+  const updates = _.pick(req.body, [
+    'employeeNumber',
+    'studentNumber',
+    'username',
+  ])
+
+  Object.assign(user, updates)
+
+  await user.save()
+
+  return res.send(200)
+}
+
+const router = Router()
+
+router.put('/courseRealisation/:feedbackTargetId', updateCourseRealisation)
+router.put('/courseRealisations', updateManyCourseRealisations)
+router.put('/enableCourses', enableAllCourses)
+router.put('/user/:userId', updateUser)
+
+module.exports = router
---
+++ src/server/routes/users/userController.js
@@ -0,0 +1,78 @@
+const { Op } = require('sequelize')
+
+const { Router } = require('express')
+const { ApplicationError } = require('../../util/customErrors')
+const { User } = require('../../models')
+const { ADMINS } = require('../../../config')
+
+const getUser = async (req, res) => {
+  const { user, isAdmin } = req
+
+  if (!user) throw new ApplicationError('Not found', 404)
+
+  const isTeacher = !!user.employeeNumber
+
+  return res.send({
+    ...user.toJSON(),
+    isTeacher,
+    iamGroups: user.iamGroups ?? [],
+    isAdmin,
+  })
+}
+
+const getUserByEmail = async (req, res) => {
+  const {
+    query: { email },
+  } = req
+
+  const params = { email }
+
+  const persons = await User.findAll({
+    attributes: ['id', 'firstName', 'lastName', 'email', 'secondaryEmail'],
+    where: {
+      [Op.or]: {
+        email: { [Op.iLike]: `${email}%` },
+        secondaryEmail: { [Op.iLike]: `${email}%` },
+      },
+    },
+    limit: 10,
+  })
+
+  return res.send({
+    params,
+    persons,
+  })
+}
+
+const getUserDetails = async (req, res) => {
+  const { id } = req.params
+  if (id !== req.user.id && !ADMINS.includes(req.user.username)) {
+    throw new ApplicationError('Non-admin can only view own user details', 403)
+  }
+  const user = await User.findByPk(id)
+  const access = await user.getOrganisationAccess()
+  return res.send({
+    ...user.dataValues,
+    iamGroups: user.iamGroups,
+    access,
+  })
+}
+
+const logout = async (req, res) => {
+  const {
+    headers: { shib_logout_url: shibLogoutUrl },
+  } = req
+
+  return res.send({
+    url: shibLogoutUrl,
+  })
+}
+
+const router = Router()
+
+router.get('/login', getUser)
+router.get('/logout', logout)
+router.get('/users', getUserByEmail)
+router.get('/users/:id', getUserDetails)
+
+module.exports = router
