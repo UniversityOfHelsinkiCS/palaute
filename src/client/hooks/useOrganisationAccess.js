4a8d6d4a7431e684ac6c3d65b3407d0de445b1b2
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-08-16 16:12:22 +0300

    Fix undefined access

--- src/client/hooks/useOrganisationAccess.js
+++ src/client/hooks/useOrganisationAccess.js
@@ -5,7 +5,8 @@ const useOrganisationAccess = (feedbackTarget) => {
   if (isLoading || !feedbackTarget) return {}
   const orgIds = feedbackTarget?.courseUnit?.organisations.map((org) => org.id)
   return (
-    organisations.find((org) => orgIds.some((id) => id === org.id)).access || {}
+    organisations.find((org) => orgIds.some((id) => id === org.id))?.access ||
+    {}
   )
 }
 
