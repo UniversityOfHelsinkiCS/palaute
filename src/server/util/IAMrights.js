26629a08c3967520f48bd039e7e6906df6748cd7
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-08-16 10:48:52 +0300

    No organisation write access for johtoryhma

--- src/server/util/IAMrights.js
+++ src/server/util/IAMrights.js
@@ -175,6 +175,7 @@ const getProgrammeAdminAccess = (hyGroups) => {
 }
 
 /**
+ * UPDATE: nobody gets this. Kept here for documentation and consistency with other Toska software
  * Grant write and read access if the user belongs to employees group and studyprogramme's manager group
  * @param {string[]} hyGroups
  */
@@ -226,7 +227,7 @@ const getIAMRights = (hyGroups) => {
     getDoctoralAccess,
     getDoctoralSchoolAccess,
     getProgrammeReadAccess,
-    getProgrammeWriteAccess,
+    // getProgrammeWriteAccess,
     getProgrammeAdminAccess,
     getFacultyAdminRights,
     getAdmin,
