19ffb5b3fdc1652b287dc4ecc7dca4269e058a5b
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-09-25 13:30:11 +0300

    Add index based on username. How did we not have this already lol

---
+++ src/server/migrations/20220925_00_add_index_based_on_username_to_user.js
@@ -0,0 +1,10 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.addIndex('users', {
+      fields: ['username'],
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropIndex('users', ['usernamne'])
+  },
+}
