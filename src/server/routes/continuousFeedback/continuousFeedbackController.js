e25ea776c04f58db0bb7c24f830f9734f10afa8c
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-09-26 09:47:15 +0300

    Create route to submit continuous Feedback

--- src/server/routes/continuousFeedback/continuousFeedbackController.js
+++ src/server/routes/continuousFeedback/continuousFeedbackController.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
-const { ContinuousFeedback } = require('../../models')
+const { ContinuousFeedback, FeedbackTarget } = require('../../models')
 const { adminAccess } = require('../../middleware/adminAccess')
+const { ApplicationError } = require('../../util/customErrors')
 
 const getFeedbacks = async (req, res) => {
   const feedbackTargetId = Number(req.params.id)
@@ -14,8 +15,31 @@ const getFeedbacks = async (req, res) => {
   return res.send(continuousFeedbacks)
 }
 
+const submitFeedback = async (req, res) => {
+  const { user } = req.user
+
+  if (!user) return res.send([])
+
+  const feedbackTargetId = Number(req.params.id)
+  const { feedback } = req.body
+
+  const feedbackTarget = await FeedbackTarget.findByPk(feedbackTargetId)
+
+  if (!feedbackTarget.continuousFeedbackEnabled)
+    throw new ApplicationError('Continuous feedback is disabled', 400)
+
+  const newFeedback = await ContinuousFeedback.create({
+    data: feedback,
+    feedbackTargetId,
+    userId: user.id,
+  })
+
+  return res.send(newFeedback)
+}
+
 const router = Router()
 
 router.get('/:id', getFeedbacks, adminAccess)
+router.post('/:id', submitFeedback)
 
 module.exports = router
