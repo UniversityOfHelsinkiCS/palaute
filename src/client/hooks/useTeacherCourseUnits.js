bdc9caf9a7dea64a47e5accfe6a813e9e3d65d6d
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-09-27 12:48:37 +0300

    Sort courses by name in copy dialog

--- src/client/hooks/useTeacherCourseUnits.js
+++ src/client/hooks/useTeacherCourseUnits.js
@@ -1,16 +1,21 @@
+import _ from 'lodash'
+import { useTranslation } from 'react-i18next'
 import { useQuery } from 'react-query'
 
 import apiClient from '../util/apiClient'
+import { getLanguageValue } from '../util/languageUtils'
 
 const defaultCacheTime = 900000
 
 const useTeacherCourseUnits = (options = {}) => {
+  const { i18n } = useTranslation()
+
   const queryKey = 'teacherCourseUnits'
 
   const queryFn = async () => {
     const { data } = await apiClient.get('/course-units/responsible')
 
-    return data
+    return _.sortBy(data, (cu) => getLanguageValue(cu.name, i18n.language))
   }
 
   const { data: courseUnits, ...rest } = useQuery(queryKey, queryFn, {
