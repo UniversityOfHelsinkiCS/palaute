11481204bf5a8db9820def86b9d0fd56999663dd
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-09-29 10:39:56 +0300

    Use new feedback results page for noad users

--- src/client/components/GuestUser/GuestFeedbackTargetResults.js
+++ src/client/components/GuestUser/GuestFeedbackTargetResults.js
@@ -2,26 +2,45 @@ import React from 'react'
 import { useParams, Redirect } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
-import { Box, Alert } from '@mui/material'
+import { Box, Alert, useMediaQuery } from '@mui/material'
 
 import useFeedbackTarget from '../../hooks/useFeedbackTarget'
 import useFeedbackTargetFeedbacks from '../../hooks/useFeedbackTargetFeedbacks'
-import FeedbackSummary from '../QuestionResults/FeedbackSummary'
-import QuestionResults from '../QuestionResults'
-import FeedbackResponse from '../FeedbackTargetResults/FeedbackResponse'
+import QuestionResults from '../FeedbackTargetResultsNew/QuestionResults'
+import FeedbackResponse from '../FeedbackTargetResultsNew/FeedbackResponse'
 
 import feedbackTargetIsOpen from '../../util/feedbackTargetIsOpen'
 import { LoadingProgress } from '../LoadingProgress'
+import FeedbackChart from '../FeedbackTargetResultsNew/QuestionResults/FeedbackChart'
 
-const GuestFeedbackTargetResults = () => {
+const NotEnoughFeedbacks = ({ t }) => (
+  <Box mb={2}>
+    <Alert severity="warning" data-cy="notEnoughFeedbacks">
+      {t('feedbackTargetResults:notEnoughFeedbacksInfo')}
+    </Alert>
+  </Box>
+)
+
+const OnlyForEnrolled = ({ t }) => (
+  <Box mb={2}>
+    <Alert severity="warning">
+      {t('feedbackTargetResults:onlyForEnrolledInfo')}
+    </Alert>
+  </Box>
+)
+
+const FeedbackTargetResultsView = () => {
   const { t } = useTranslation()
   const { id } = useParams()
+  const isMobileChrome =
+    useMediaQuery('(max-width:500px)') &&
+    navigator.userAgent?.toLowerCase()?.indexOf('chrome') !== -1
 
   const { feedbackTarget, isLoading: feedbackTargetIsLoading } =
-    useFeedbackTarget(id, { skipCache: true })
+    useFeedbackTarget(id)
 
   const { feedbackTargetData, isLoading: feedbacksIsLoading } =
-    useFeedbackTargetFeedbacks(id, { skipCache: true })
+    useFeedbackTargetFeedbacks(id)
 
   const isLoading = feedbackTargetIsLoading || feedbacksIsLoading
 
@@ -35,31 +54,28 @@ const GuestFeedbackTargetResults = () => {
 
   const { feedbacks, feedbackVisible } = feedbackTargetData
 
-  const { questions, publicQuestionIds, feedback } = feedbackTarget
+  const {
+    questions,
+    publicQuestionIds,
+    feedback,
+    studentCount,
+    feedbackCount,
+    opensAt,
+    closesAt,
+    feedbackReminderLastSentAt,
+  } = feedbackTarget
 
   const isOpen = feedbackTargetIsOpen(feedbackTarget)
 
+  if (isOpen && !feedback) {
+    return <Redirect to={`noad/targets/${feedbackTarget.id}/feedback`} />
+  }
+
   const feedbackHasStarted = new Date(feedbackTarget.opensAt) < new Date()
 
-  const notEnoughFeedbacksAlert = (
-    <Box mb={2}>
-      <Alert severity="warning">
-        {t('feedbackTargetResults:notEnoughFeedbacksInfo')}
-      </Alert>
-    </Box>
-  )
-
-  const onlyForEnrolledAlert = (
-    <Box mb={2}>
-      <Alert severity="warning">
-        {t('feedbackTargetResults:onlyForEnrolledInfo')}
-      </Alert>
-    </Box>
-  )
-
   return (
-    <>
-      {feedbackHasStarted && !isOpen && (
+    <Box>
+      {feedbackHasStarted && !isOpen && feedbacks.length > 0 && (
         <Box mb={2}>
           <FeedbackResponse feedbackTarget={feedbackTarget} />
         </Box>
@@ -73,32 +89,48 @@ const GuestFeedbackTargetResults = () => {
         </Box>
       )}
 
-      {feedbacks.length === 0 && feedbackVisible && notEnoughFeedbacksAlert}
-
-      {feedbacks.length === 0 && !feedbackVisible && onlyForEnrolledAlert}
-
-      {feedbacks.length !== 0 && (
-        <Box mb={2}>
-          <FeedbackSummary
-            publicQuestionIds={publicQuestionIds ?? []}
-            questions={questions}
+      {!isMobileChrome && (
+        <Box>
+          <FeedbackChart
             feedbacks={feedbacks}
-            isTeacher={false}
+            studentCount={studentCount}
+            opensAt={opensAt}
+            closesAt={closesAt}
+            feedbackReminderLastSentAt={feedbackReminderLastSentAt}
+            t={t}
           />
         </Box>
       )}
 
-      {feedbacks.length !== 0 && (
-        <QuestionResults
-          publicQuestionIds={publicQuestionIds ?? []}
-          selectPublicQuestionsLink={`/targets/${feedbackTarget.id}/feedback-response`}
-          questions={questions}
-          feedbacks={feedbacks}
-          isTeacher={false}
-        />
+      {feedbacks.length === 0 &&
+        (feedbackVisible ? (
+          <NotEnoughFeedbacks t={t} />
+        ) : (
+          <OnlyForEnrolled t={t} />
+        ))}
+
+      {feedbacks.length > 0 && (
+        <Box>
+          <QuestionResults
+            publicQuestionIds={publicQuestionIds ?? []}
+            selectPublicQuestionsLink={`/targets/${feedbackTarget.id}/settings`}
+            questions={questions}
+            feedbacks={feedbacks}
+            isTeacher={false}
+            organisationAccess={false}
+            feedbackCount={feedbackCount}
+            feedbackTargetId={id}
+          />
+        </Box>
       )}
-    </>
+    </Box>
   )
 }
+
+const GuestFeedbackTargetResults = () => (
+  <Box>
+    <FeedbackTargetResultsView />
+  </Box>
+)
 
 export default GuestFeedbackTargetResults
