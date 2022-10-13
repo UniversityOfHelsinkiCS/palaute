83244f7e6965b743f84bbc18cfaf23c51f96335f
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-10-13 17:54:25 +0300

    Migrate feedback correspondents again because first time fakdup

--- src/server/migrations/20221013_00_init_organisation_feedback_correspondents.js
+++ src/server/migrations/20221013_00_init_organisation_feedback_correspondents.js
@@ -35,44 +35,6 @@ module.exports = {
         },
         { transaction },
       )
-
-      try {
-        console.log('CHECKING')
-        await Organisation.findOne({ attributes: ['responsibleUserId'] })
-      } catch (error) {
-        console.log(
-          'Organisations dont have responsible user id, skipping data migration',
-        )
-        return
-      }
-
-      const organisations = await queryInterface.sequelize.query(
-        `
-        SELECT id, responsible_user_id as "responsibleUserId"
-        FROM organisations
-        WHERE responsible_user_id IS NOT NULL;
-      `,
-        { type: QueryTypes.SELECT },
-      )
-
-      for (const org of organisations) {
-        await queryInterface.sequelize.query(
-          `
-          INSERT INTO organisation_feedback_correspondents 
-          (user_id, organisation_id, created_at, updated_at) 
-          VALUES (:userId, :organisationId, :createdAt, :updatedAt);
-        `,
-          {
-            replacements: {
-              userId: org.responsibleUserId,
-              organisationId: org.id,
-              createdAt: new Date(),
-              updatedAt: new Date(),
-            },
-            transaction,
-          },
-        )
-      }
     })
   },
   down: async (queryInterface) => {
---
+++ src/server/migrations/20221013_01_migrate_feedback_correspondents.js
@@ -0,0 +1,62 @@
+const { QueryTypes } = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      try {
+        console.log('CHECKING')
+        await queryInterface.sequelize.query(`
+          SELECT responsible_user_id FROM organisations LIMIT 1;
+        `)
+      } catch (error) {
+        console.log(
+          'Organisations dont have responsible user id, skipping data migration',
+        )
+        return
+      }
+
+      const organisations = await queryInterface.sequelize.query(
+        `
+        SELECT id, responsible_user_id as "responsibleUserId"
+        FROM organisations
+        WHERE responsible_user_id IS NOT NULL;
+      `,
+        { type: QueryTypes.SELECT },
+      )
+
+      for (const org of organisations) {
+        // check for existing
+        const users = await queryInterface.sequelize.query(
+          `
+          SELECT user_id as "userId" FROM organisation_feedback_correspondents
+          WHERE organisation_id = :organisationId;
+        `,
+          { type: QueryTypes.SELECT, replacements: { organisationId: org.id } },
+        )
+
+        if (users.map((u) => u.userId).includes(org.responsibleUserId)) continue
+
+        await queryInterface.sequelize.query(
+          `
+          INSERT INTO organisation_feedback_correspondents 
+          (user_id, organisation_id, created_at, updated_at) 
+          VALUES (:userId, :organisationId, :createdAt, :updatedAt);
+        `,
+          {
+            type: QueryTypes.INSERT,
+            replacements: {
+              userId: org.responsibleUserId,
+              organisationId: org.id,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+            transaction,
+          },
+        )
+      }
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('organisation_feedback_correspondents')
+  },
+}
