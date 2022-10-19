e1b5cdcf1c6a777a28f56758c9aebbc860408cdf
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-10-19 14:42:09 +0300

    Slightly faster updating with async requests

--- src/server/updater/updateLooper.js
+++ src/server/updater/updateLooper.js
@@ -12,38 +12,48 @@ const getData = async (limit, offset, url) => {
 
 const mangleData = async (url, limit, handler) => {
   logger.info(`[UPDATER] Starting to update items with url ${url}`)
+  const start = new Date()
+  let requestStart = null
+  let loopStart = null
+
   let offset = 0
   let count = 0
-  const start = new Date()
+  let currentData = null
+  let nextData = null
+
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    let requestTime = null
-    let data = null
     try {
-      const requestStart = Date.now()
-      data = await getData(limit, offset, url)
-      requestTime = (Date.now() - requestStart).toFixed(0)
+      currentData = await nextData
+      if (currentData?.length === 0) break
+
+      const requestTime = (Date.now() - requestStart).toFixed(0)
+      requestStart = Date.now()
+
+      nextData = getData(limit, offset, url)
+
+      if (currentData) {
+        const processingStart = Date.now()
+        await handler(currentData)
+        const processingTime = (Date.now() - processingStart).toFixed(0)
+        const totalTime = (Date.now() - loopStart).toFixed(0)
+        loopStart = Date.now()
+        logger.info(`[UPDATERLOOP]`, {
+          offset,
+          items: currentData.length,
+          requestTime,
+          processingTime,
+          totalTime,
+        })
+        count += currentData.length
+        offset += limit
+      }
     } catch (e) {
       logger.info('[UPDATER] ERROR:')
       logger.error(e)
     }
-    // eslint-disable-next-line no-continue
-    if (data === null) continue
-    if (data.length === 0) break
-
-    const processingStart = Date.now()
-    await handler(data)
-    const processingTime = (Date.now() - processingStart).toFixed(0)
-
-    logger.info(`[UPDATERLOOP]`, {
-      offset,
-      items: data.length,
-      requestTime,
-      processingTime,
-    })
-    count += data.length
-    offset += limit
   }
+
   const duration = new Date() - start
   logger.info(
     `[UPDATER] Updated ${count} items at ${(duration / count).toFixed(
