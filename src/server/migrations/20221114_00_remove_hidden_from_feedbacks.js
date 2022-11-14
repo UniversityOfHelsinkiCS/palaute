eafbb44b12f08c3012ff4edbb373ca3f44c8e2ed
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2022-11-14 23:09:19 +0200

    Remove hidden col from feedbacks (was a mistake adding it)

---
+++ src/server/migrations/20221114_00_remove_hidden_from_feedbacks.js
@@ -0,0 +1,14 @@
+const { BOOLEAN } = require('sequelize')
+
+// :lul:
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.removeColumn('feedbacks', 'hidden')
+  },
+  down: async (queryInterface) => {
+    await queryInterface.addColumn('feedbacks', 'hidden', {
+      type: BOOLEAN,
+      defaultValue: false,
+    })
+  },
+}
