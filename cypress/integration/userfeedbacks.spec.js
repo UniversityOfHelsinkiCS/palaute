52cc9a9c27c165a116af3069788005f04ecbc81c
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2022-11-15 16:12:08 +0200

    Test that feedback censoring worx

--- cypress/integration/userfeedbacks.spec.js
+++ cypress/integration/userfeedbacks.spec.js
@@ -1,10 +1,12 @@
+const { baseUrl } = require('../support')
+
 describe('User feedbacks view', () => {
-  beforeEach(() => {
+  it('A feedback is visible after teacher has set it active', () => {
+    // setup
     cy.loginAsSecondaryTeacher()
     cy.setFeedbackActive()
     cy.loginAsStudent()
-  })
-  it('A feedback is visible after teacher has set it active', () => {
+
     cy.contains('Functional Programming I')
     cy.get('[data-cy=giveCourseFeedback]').click()
     cy.contains(
@@ -17,7 +19,23 @@ describe('User feedbacks view', () => {
     cy.get('[data-cy=submitFeedbackButton]').click()
     cy.contains('Feedback has been given. Thank you for your feedback!')
   })
+  it('Teacher can censor a feedback', () => {
+    cy.loginAsSecondaryTeacher()
+    cy.visit(`${baseUrl}/targets/163/results`)
+    cy.get('[data-testid="VisibilityIcon"]').click()
+    cy.contains('This feedback is hidden')
+
+    // it is now hidden from student
+    cy.loginAsStudent()
+    cy.visit(`${baseUrl}/targets/163/results`)
+    cy.contains('Other comments and such').should('not.exist')
+  })
   it('Student can clear given feedback', () => {
+    // setup
+    cy.loginAsSecondaryTeacher()
+    cy.setFeedbackActive()
+    cy.loginAsStudent()
+
     cy.loginAsStudent()
     cy.contains('Given').click()
     cy.contains('Remove my feedback').click()
