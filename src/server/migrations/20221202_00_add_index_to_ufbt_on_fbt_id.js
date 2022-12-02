c0a20560dd98927710a0306551266bbf00c2d19a
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-12-02 16:59:00 +0200

    Add index to ufbt on fbt id

---
+++ src/server/migrations/20221202_00_add_index_to_ufbt_on_fbt_id.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+      CREATE INDEX user_feedback_targets_feedback_target_id
+      ON user_feedback_targets (feedback_target_id);
+    `)
+  },
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+      DROP INDEX user_feedback_targets_feedback_target_id;
+    `)
+  },
+}
