77ecd894e125aed4a65c7041addb04f874c1ac55
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-01-09 14:34:02 +0200

    Fix logged-in-as banner not being visible in production

--- src/client/components/DevTools/index.js
+++ src/client/components/DevTools/index.js
@@ -4,15 +4,11 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import { inProduction } from '../../util/common'
 import AdminLoggedInAsBanner from './AdminLoggedInAsBanner'
 
-const DevTools = () => {
-  if (inProduction) return null
-
-  return (
-    <>
-      <ReactQueryDevtools position="bottom-right" />
-      <AdminLoggedInAsBanner />
-    </>
-  )
-}
+const DevTools = () => (
+  <>
+    {!inProduction && <ReactQueryDevtools position="bottom-right" />}
+    <AdminLoggedInAsBanner />
+  </>
+)
 
 export default DevTools
