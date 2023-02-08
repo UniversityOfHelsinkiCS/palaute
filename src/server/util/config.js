ab9ca48ab05365e578be1970ff3254383324f4c0
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-02-08 13:38:03 +0200

    No ssl in production, only staging

--- src/server/util/config.js
+++ src/server/util/config.js
@@ -23,7 +23,7 @@ const useOldImporter = false
 
 let DB_CONNECTION_STRING = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}?targetServerType=primary`
 
-if (inProduction || inStaging) DB_CONNECTION_STRING = `${DB_CONNECTION_STRING}&ssl=true`
+if (inStaging) DB_CONNECTION_STRING = `${DB_CONNECTION_STRING}&ssl=true`
 
 const WORKLOAD_QUESTION_ID = Number(config.get('WORKLOAD_QUESTION_ID'))
 const ADMINS = config.get('ADMINS') ?? []
