1a20cea9be173ba8c199d1fcc9c8c1565dbafe33
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-02-17 15:24:34 +0200

    Try fixing translation file url

--- src/client/util/i18n.js
+++ src/client/util/i18n.js
@@ -1,22 +1,28 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
-import { inProduction, TRANSLATION_NAMESPACE } from './common'
+import { basePath, inProduction, TRANSLATION_NAMESPACE } from './common'
 
 //import en from '../locales/en'
 //import fi from '../locales/fi'
 //import sv from '../locales/sv'
 
-i18n.use(initReactI18next).use(HttpApi).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  defaultNS: TRANSLATION_NAMESPACE,
-  fallbackNS: 'translation',
-  load: 'languageOnly',
-  debug: !inProduction,
-  nsSeparator: '.',
-  keySeparator: ':',
-})
+i18n
+  .use(initReactI18next)
+  .use(HttpApi)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    defaultNS: TRANSLATION_NAMESPACE,
+    fallbackNS: 'translation',
+    load: 'languageOnly',
+    backend: {
+      loadPath: `${basePath}/locales/{{lng}}/{{ns}}.json`,
+    },
+    debug: !inProduction,
+    nsSeparator: '.',
+    keySeparator: ':',
+  })
 
 // eslint-disable-next-line
 window.__i18n__ = i18n
