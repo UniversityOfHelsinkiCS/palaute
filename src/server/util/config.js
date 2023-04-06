10346e80615de6352835cc99f003043bfffc9c61
Mika Nevalainen <mikanevalainen@Mikas-MacBook-Pro.local>
2023-04-06 16:55:42 +0300

    Use env variable for setting optional SSL-connection for db

--- src/server/util/config.js
+++ src/server/util/config.js
@@ -19,7 +19,7 @@ const UPDATER_URL = process.env.UPDATER_URL || ''
 
 let DB_CONNECTION_STRING = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}?targetServerType=primary`
 
-if (inStaging) DB_CONNECTION_STRING = `${DB_CONNECTION_STRING}&ssl=true`
+if (process.env.POSTGRES_SSL) DB_CONNECTION_STRING = `${DB_CONNECTION_STRING}&ssl=true`
 
 const WORKLOAD_QUESTION_ID = Number(config.get('WORKLOAD_QUESTION_ID'))
 const DEV_ADMINS = config.get('DEV_ADMINS') ?? []
