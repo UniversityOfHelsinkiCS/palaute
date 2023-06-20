3d61f6558bad702bd9e900bed4d455e8e06a832c
Pasi Heiskanen <pasi.heiskanen@leanware.fi>
2023-06-20 14:27:13 +0300

    Keep selected language after page refresh. Requires package i18next-browser-languagedetector

--- src/client/util/i18n.js
+++ src/client/util/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { basePath, inProduction, TRANSLATION_NAMESPACE } from './common'
 
 /**
@@ -13,8 +14,8 @@ import { basePath, inProduction, TRANSLATION_NAMESPACE } from './common'
 i18n
   .use(initReactI18next)
   .use(HttpApi)
+  .use(LanguageDetector)
   .init({
-    lng: 'en',
     fallbackLng: 'en',
     defaultNS: TRANSLATION_NAMESPACE,
     fallbackNS: 'translation',
