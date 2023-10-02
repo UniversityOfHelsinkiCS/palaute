55b3f2e7f72d9cf0e4e6b1a7f242bc624943dc75
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-02 17:26:52 +0300

    feat(summary-v2): Store summary opts in query string

--- src/client/pages/CourseSummary/SummaryV2/context.js
+++ src/client/pages/CourseSummary/SummaryV2/context.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import useHistoryState from '../../../hooks/useHistoryState'
+import { format, isValid } from 'date-fns'
+import useURLSearchParams from '../../../hooks/useURLSearchParams'
 
 const summaryContext = React.createContext({
   showSummariesWithNoFeedback: false,
   setShowSummariesWithNoFeedback: () => {},
   dateRange: {
-    startDate: new Date('2023-01-01'),
-    endDate: new Date('2024-01-01'),
+    start: new Date('2023-01-01'),
+    end: new Date('2024-01-01'),
   },
   setDateRange: () => {},
   option: 'year',
@@ -14,21 +15,60 @@ const summaryContext = React.createContext({
 })
 
 export const SummaryContextProvider = ({ children }) => {
-  const [showSummariesWithNoFeedback, setShowSummariesWithNoFeedback] = React.useState(false)
-  const [dateRange, setDateRange] = useHistoryState('summary-v2-time-range', {
-    start: new Date('2023-01-01'),
-    end: new Date('2024-01-01'),
+  const [params, setParams] = useURLSearchParams()
+
+  const [showSummariesWithNoFeedback, setShowSummariesWithNoFeedback] = React.useState(() => {
+    const showSummariesWithNoFeedbackParam = params.get('showSummariesWithNoFeedback')
+    return showSummariesWithNoFeedbackParam ? showSummariesWithNoFeedbackParam === 'true' : false
   })
-  const [option, setOption] = React.useState('year')
+
+  const updateShowSummariesWithNoFeedbackQS = React.useCallback(showSummariesWithNoFeedback => {
+    setShowSummariesWithNoFeedback(showSummariesWithNoFeedback)
+    params.set('showSummariesWithNoFeedback', showSummariesWithNoFeedback)
+    setParams(params)
+  })
+
+  const [dateRange, setDateRange] = React.useState(() => {
+    // Converting to string is important, params.get may return 0 which would take us to the 70s
+    const start = new Date(String(params.get('startDate')))
+    const end = new Date(String(params.get('endDate')))
+
+    return isValid(start) && isValid(end)
+      ? { start, end }
+      : {
+          start: new Date('2023-01-01'),
+          end: new Date('2024-01-01'),
+        }
+  })
+
+  const updateDateRangeQS = React.useCallback(dateRange => {
+    setDateRange(dateRange)
+    if (isValid(dateRange.start) && isValid(dateRange.end)) {
+      params.set('startDate', format(dateRange.start, 'yyyy-MM-dd'))
+      params.set('endDate', format(dateRange.end, 'yyyy-MM-dd'))
+      setParams(params)
+    }
+  })
+
+  const [option, setOption] = React.useState(() => {
+    const option = params.get('option')
+    return option || 'year'
+  })
+
+  const updateOptionQS = React.useCallback(option => {
+    setOption(option)
+    params.set('option', option)
+    setParams(params)
+  })
 
   const value = React.useMemo(
     () => ({
       showSummariesWithNoFeedback,
-      setShowSummariesWithNoFeedback,
+      setShowSummariesWithNoFeedback: updateShowSummariesWithNoFeedbackQS,
       dateRange,
-      setDateRange,
+      setDateRange: updateDateRangeQS,
       option,
-      setOption,
+      setOption: updateOptionQS,
     }),
     [showSummariesWithNoFeedback, dateRange, option]
   )
