7e3f5ae7d5d3502d7ff9c42ec0c7c856002d8040
Henri Remonen <henri@remonen.fi>
2023-10-05 14:14:42 +0300

    feat: Create form for the organisation survey

--- src/client/pages/Organisation/OrganisationSurveys.js
+++ src/client/pages/Organisation/OrganisationSurveys.js
@@ -1,26 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Box } from '@mui/material'
+import { Card, CardContent, Box, Button, Typography } from '@mui/material'
+import { Form, Formik } from 'formik'
 
 import { useParams } from 'react-router-dom'
 
 import useOrganisation from '../../hooks/useOrganisation'
 
+import Title from '../../components/common/Title'
+import FormikTextField from '../../components/common/FormikTextField'
 import { LoadingProgress } from '../../components/common/LoadingProgress'
+import FormikDatePicker from '../../components/common/FormikDatePicker'
+
+const OrganisationSurveyForm = ({ isOpen, close, handleSubmit }) => {
+  const { t } = useTranslation()
+
+  const initialValues = {
+    name: '',
+    startDate: new Date(),
+    endDate: new Date(),
+  }
+
+  if (!isOpen) return null
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validateOnChange={false}>
+      {({ isSubmitting }) => {
+        const disabled = isSubmitting
+
+        return (
+          <Form>
+            <Card>
+              <CardContent>
+                <FormikTextField name="name" label="Name of the survey" id="name" fullWidth />
+                <FormikDatePicker name="startDate" label="Start date" id="startDate" />
+                <FormikDatePicker name="endDate" label="End date" id="endDate" />
+              </CardContent>
+            </Card>
+
+            <Box mt={2}>
+              <Button disabled={disabled} color="primary" variant="contained" type="submit">
+                {t('common:save')}
+              </Button>
+              <Button color="error" variant="contained" type="button" onClick={close}>
+                {t('common:hide')}
+              </Button>
+            </Box>
+          </Form>
+        )
+      }}
+    </Formik>
+  )
+}
 
 const OrganisationSurveys = () => {
+  const [showForm, setShowForm] = useState(false)
   const { code } = useParams()
   const { organisation, isLoading: isOrganisationLoading } = useOrganisation(code)
   const { t } = useTranslation()
+
+  const handleSubmit = async values => {
+    setShowForm(!showForm)
+    console.log(values)
+  }
+
+  const handleClose = () => setShowForm(!showForm)
 
   if (isOrganisationLoading) {
     return <LoadingProgress />
   }
 
-  console.log(organisation)
+  return (
+    <>
+      <Title>{t('common:courseSummaryPage')}</Title>
+      <Box mb={6} px={1}>
+        <Box display="flex" gap="1rem" alignItems="end">
+          <Typography variant="h4" component="h1">
+            {t('organisationSurveys:heading')}
+          </Typography>
+        </Box>
+        <Box mt={4} />
 
-  return <Box mb={2}>Testi</Box>
+        <OrganisationSurveyForm isOpen={showForm} close={handleClose} handleSubmit={handleSubmit} />
+
+        <Box mt={4}>
+          <Button
+            color="primary"
+            onClick={() => {
+              setShowForm(!showForm)
+            }}
+            disabled={showForm}
+          >
+            {t('organisationSurveys:addSurvey')}
+          </Button>
+        </Box>
+      </Box>
+    </>
+  )
 }
 
 export default OrganisationSurveys
