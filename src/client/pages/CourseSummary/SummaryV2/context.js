1425f1494af9a50b3077dbf2b581b21aaa574de3
Veikkosuhonen <vesuvesuh@gmail.com>
2023-11-01 14:44:18 +0200

    feat(summary-v2): Add sort&order to context & url params

--- src/client/pages/CourseSummary/SummaryV2/context.js
+++ src/client/pages/CourseSummary/SummaryV2/context.js
@@ -12,10 +12,14 @@ const summaryContext = React.createContext({
   setDateRange: () => {},
   option: 'year',
   setOption: () => {},
+  sortBy: ['code', 'asc'],
+  setSortBy: () => {},
 })
 
 export const SummaryContextProvider = ({ children }) => {
   const [params, setParams] = useURLSearchParams()
+
+  // Whether to show summaries with no feedback
 
   const [showSummariesWithNoFeedback, setShowSummariesWithNoFeedback] = React.useState(() => {
     const showSummariesWithNoFeedbackParam = params.get('showSummariesWithNoFeedback')
@@ -27,6 +31,8 @@ export const SummaryContextProvider = ({ children }) => {
     params.set('showSummariesWithNoFeedback', showSummariesWithNoFeedback)
     setParams(params)
   })
+
+  // Date range
 
   const [dateRange, setDateRange] = React.useState(() => {
     // Converting to string is important, params.get may return 0 which would take us to the 70s
@@ -50,6 +56,8 @@ export const SummaryContextProvider = ({ children }) => {
     }
   })
 
+  // Option: year or semester
+
   const [option, setOption] = React.useState(() => {
     const option = params.get('option')
     return option || 'year'
@@ -61,6 +69,21 @@ export const SummaryContextProvider = ({ children }) => {
     setParams(params)
   })
 
+  // Sort by
+
+  const [sortBy, setSortBy] = React.useState(() => {
+    const sortByField = params.get('sortBy')
+    const order = params.get('order')
+    return sortByField && order ? [sortByField, order] : ['code', 'asc']
+  })
+
+  const updateSortByQS = React.useCallback(sortBy => {
+    setSortBy(sortBy)
+    params.set('sortBy', sortBy[0])
+    params.set('order', sortBy[1])
+    setParams(params)
+  })
+
   const value = React.useMemo(
     () => ({
       showSummariesWithNoFeedback,
@@ -69,8 +92,10 @@ export const SummaryContextProvider = ({ children }) => {
       setDateRange: updateDateRangeQS,
       option,
       setOption: updateOptionQS,
+      sortBy,
+      setSortBy: updateSortByQS,
     }),
-    [showSummariesWithNoFeedback, dateRange, option]
+    [showSummariesWithNoFeedback, dateRange, option, sortBy[0], sortBy[1]]
   )
 
   return <summaryContext.Provider value={value}>{children}</summaryContext.Provider>
