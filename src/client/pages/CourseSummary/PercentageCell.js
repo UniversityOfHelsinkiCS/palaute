ddc2389e5c408b284aeed27c89319c43b27b51f3
Henri Remonen <henri@remonen.fi>
2023-11-03 10:44:14 +0200

    feat: update the percentage cell to allow for additional props

--- src/client/pages/CourseSummary/PercentageCell.js
+++ src/client/pages/CourseSummary/PercentageCell.js
@@ -14,7 +14,7 @@ const styles = {
  * @param {{ string, number }} { label, percent }
  * @returns
  */
-const PercentageCell = ({ label, tooltip, percent, sx }) => {
+const PercentageCell = ({ label, tooltip, percent, sx, ...rest }) => {
   let hex = Number(percent * 2.55).toString(16)
   const indexOfDot = hex.indexOf('.')
   hex = indexOfDot === -1 ? hex : hex.substring(0, indexOfDot)
@@ -24,6 +24,7 @@ const PercentageCell = ({ label, tooltip, percent, sx }) => {
     <Tooltip title={tooltip} disableInteractive>
       <Box sx={{ ...styles.cell, ...(sx ?? {}) }}>
         <Chip
+          {...rest}
           label={label}
           sx={{
             background: theme => `${theme.palette.info.light}${hex}`,
