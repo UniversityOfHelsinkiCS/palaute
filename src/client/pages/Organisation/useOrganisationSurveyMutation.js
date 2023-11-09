68124e8b6820f09a3cad61810ee65025e1daf746
Henri Remonen <henri@remonen.fi>
2023-11-09 13:33:00 +0200

    feat: enable studentNumber edits

--- src/client/pages/Organisation/useOrganisationSurveyMutation.js
+++ src/client/pages/Organisation/useOrganisationSurveyMutation.js
@@ -28,11 +28,12 @@ export const useCreateOrganisationSurveyMutation = organisationCode => {
 }
 
 export const useEditOrganisationSurveyMutation = organisationCode => {
-  const mutationFn = async ({ surveyId, name, startDate, endDate, teacherIds }) => {
+  const mutationFn = async ({ surveyId, name, startDate, endDate, studentNumbers, teacherIds }) => {
     const { data } = await apiClient.put(`/organisations/${organisationCode}/surveys/${surveyId}`, {
       name,
       startDate,
       endDate,
+      studentNumbers,
       teacherIds,
     })
 
