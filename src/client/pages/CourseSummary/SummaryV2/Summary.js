ddb502d0205fccbdf5f6dac2891203b047471a80
Veikkosuhonen <vesuvesuh@gmail.com>
2023-11-14 15:31:41 +0200

    refactor(summary-v2): Viewing access betterment

--- src/client/pages/CourseSummary/SummaryV2/Summary.js
+++ src/client/pages/CourseSummary/SummaryV2/Summary.js
@@ -8,7 +8,6 @@ import { SummaryContextProvider } from './context'
 import useAuthorizedUser from '../../../hooks/useAuthorizedUser'
 import ProtectedRoute from '../../../components/common/ProtectedRoute'
 import MyOrganisations from './MyOrganisations'
-import { inProduction } from '../../../util/common'
 import University from './University'
 import { updateSummaries } from './api'
 import LinkButton from '../../../components/common/LinkButton'
@@ -29,6 +28,9 @@ const SummaryInContext = () => {
 
   const { authorizedUser: user } = useAuthorizedUser()
 
+  const hasAccessToMyOrganisations = Object.keys(user?.organisationAccess ?? {}).length > 0
+  const hasAccessToUniversityLevel = user?.specialGroup?.allProgrammes || user?.specialGroup?.hyOne || user?.isAdmin
+
   return (
     <>
       <Box mb="6rem" px={1}>
@@ -38,25 +40,31 @@ const SummaryInContext = () => {
           </Typography>
           <LinkButton to="/course-summary" title="Vanha" />
         </Box>
-        <Button variant="text" onClick={handleUpdateData}>
-          Aja datanpäivitys
-        </Button>
+        {user?.isAdmin && (
+          <Button variant="text" onClick={handleUpdateData}>
+            Aja datanpäivitys
+          </Button>
+        )}
       </Box>
       <RouterTabs variant="scrollable" scrollButtons="auto">
-        <RouterTab
-          label={t('common:university')}
-          icon={
-            <Box sx={{ width: '1.5rem', height: 'auto' }}>
-              <img src={hyLogo} alt="HY" />
-            </Box>
-          }
-          to={`/course-summary/v2/university${search}`}
-        />
-        <RouterTab
-          label={t('courseSummary:myOrganisations')}
-          icon={<BarChartOutlined />}
-          to={`/course-summary/v2/my-organisations${search}`}
-        />
+        {hasAccessToUniversityLevel && (
+          <RouterTab
+            label={t('common:university')}
+            icon={
+              <Box sx={{ width: '1.5rem', height: 'auto' }}>
+                <img src={hyLogo} alt="HY" />
+              </Box>
+            }
+            to={`/course-summary/v2/university${search}`}
+          />
+        )}
+        {hasAccessToMyOrganisations && (
+          <RouterTab
+            label={t('courseSummary:myOrganisations')}
+            icon={<BarChartOutlined />}
+            to={`/course-summary/v2/my-organisations${search}`}
+          />
+        )}
         <RouterTab
           label={t('courseSummary:myCourses')}
           icon={<School />}
@@ -68,27 +76,13 @@ const SummaryInContext = () => {
           <ProtectedRoute
             path="/course-summary/v2/my-organisations"
             component={MyOrganisations}
-            hasAccess={
-              // TODO: do not use HY specific special groups
-              !inProduction ||
-              user.isAdmin ||
-              user?.specialGroup?.allProgrammes ||
-              user?.specialGroup?.hyOne ||
-              user?.specialGroup?.admin
-            }
+            hasAccess={hasAccessToMyOrganisations}
           />
 
           <ProtectedRoute
             path="/course-summary/v2/university"
             component={University}
-            hasAccess={
-              // TODO: do not use HY specific special groups
-              !inProduction ||
-              user.isAdmin ||
-              user?.specialGroup?.allProgrammes ||
-              user?.specialGroup?.hyOne ||
-              user?.specialGroup?.admin
-            }
+            hasAccess={hasAccessToUniversityLevel}
           />
 
           <ProtectedRoute path="/course-summary/v2/my-courses" component={MyCourses} hasAccess />
