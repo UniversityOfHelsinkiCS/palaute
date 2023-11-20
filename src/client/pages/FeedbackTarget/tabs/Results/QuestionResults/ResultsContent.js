e89aa202e55bb62163f09fb3e7437dbc7742e951
Henri Remonen <henri@remonen.fi>
2023-11-20 13:34:22 +0200

    fix: restore ResultsContent styling

--- src/client/pages/FeedbackTarget/tabs/Results/QuestionResults/ResultsContent.js
+++ src/client/pages/FeedbackTarget/tabs/Results/QuestionResults/ResultsContent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from '@mui/material'
 
 const ResultsContent = ({ chart, children }) => (
-  <Box>
+  <Box display="flex" flexDirection="column">
     {chart}
     {children}
   </Box>
