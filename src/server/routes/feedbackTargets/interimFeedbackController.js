9d7734d1151a768cae9e137fb3c31d8925a24bca
Henri Remonen <henri@remonen.fi>
2023-11-23 14:51:15 +0200

    feat(interim-feedback): append route for creating a new interim feedback

--- src/server/routes/feedbackTargets/interimFeedbackController.js
+++ src/server/routes/feedbackTargets/interimFeedbackController.js
@@ -1,5 +1,39 @@
 const { Router } = require('express')
 
+const {
+  createUserFeedbackTargets,
+  createInterimFeedbackTarget,
+  getFbtUserIds,
+  getInterimFeedbackById,
+} = require('../../services/feedbackTargets/interimFeedbacks')
+
+const createInterimFeedback = async (req, res) => {
+  const { user } = req
+  const { fbtId } = req.params
+  const { name, startDate, endDate } = req.body
+
+  const interimFeedbackTarget = await createInterimFeedbackTarget(fbtId, { name, startDate, endDate })
+
+  const studentIds = await getFbtUserIds(fbtId, 'STUDENT')
+  const teacherIds = await getFbtUserIds(fbtId, 'RESPONSIBLE_TEACHER')
+
+  const studentFeedbackTargets = await createUserFeedbackTargets(interimFeedbackTarget.id, studentIds, 'STUDENT')
+  const teacherFeedbackTargets = await createUserFeedbackTargets(
+    interimFeedbackTarget.id,
+    teacherIds,
+    'RESPONSIBLE_TEACHER'
+  )
+
+  const survey = await getInterimFeedbackById(interimFeedbackTarget.id)
+
+  return res.status(201).send({
+    ...survey.dataValues,
+    userFeedbackTargets: [...studentFeedbackTargets, ...teacherFeedbackTargets],
+  })
+}
+
 const router = Router()
+
+router.post('/:fbtId/interimFeedback', createInterimFeedback)
 
 module.exports = router
