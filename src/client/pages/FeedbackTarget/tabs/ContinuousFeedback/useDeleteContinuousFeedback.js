bfc2cf8f71a50be80b6ed18a8ea89c5752633606
Veikkosuhonen <vesuvesuh@gmail.com>
2023-11-29 20:16:58 +0200

    feat: Ability for admin to delete continuous feedbacks from UI

---
+++ src/client/pages/FeedbackTarget/tabs/ContinuousFeedback/useDeleteContinuousFeedback.js
@@ -0,0 +1,48 @@
+import { useSnackbar } from 'notistack'
+import { useTranslation } from 'react-i18next'
+import { useMutation } from 'react-query'
+import apiClient from '../../../../util/apiClient'
+import queryClient from '../../../../util/queryClient'
+import { useFeedbackTargetContext } from '../../FeedbackTargetContext'
+
+/**
+ * For SUPERADMIN deleting continuous feedbacks. Gets the feedback target from FeedbackTargetContext,
+ * meaning it can only be used in the context
+ */
+const useDeleteContinuousFeedback = () => {
+  const { feedbackTarget, isAdmin } = useFeedbackTargetContext()
+  const { t } = useTranslation()
+  const { enqueueSnackbar } = useSnackbar()
+
+  const mutationFn = async ({ continuousFeedbackId }) =>
+    apiClient.delete(`/continuous-feedback/${feedbackTarget.id}/${continuousFeedbackId}`)
+
+  const mutation = useMutation(mutationFn, {
+    onSuccess: () => {
+      queryClient.refetchQueries(['feedbackTargetContinuousFeedbacks', String(feedbackTarget.id)])
+    },
+  })
+
+  const canDelete = isAdmin
+
+  const deleteAnswer = async continuousFeedbackId => {
+    // eslint-disable-next-line no-alert
+    if (!canDelete || !window.confirm(t('feedbackTargetResults:confirmDeleteFeedback'))) return
+
+    try {
+      await mutation.mutateAsync({
+        continuousFeedbackId,
+      })
+      enqueueSnackbar(t('feedbackTargetResults:deleteSuccess'), { variant: 'success' })
+    } catch (e) {
+      enqueueSnackbar(t('common:unknownError'), { variant: 'error' })
+    }
+  }
+
+  return {
+    canDelete,
+    deleteAnswer,
+  }
+}
+
+export default useDeleteContinuousFeedback
