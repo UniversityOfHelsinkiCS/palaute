6fef20e837dc78a93cd54405268ac92a10134477
Henri Remonen <henri@remonen.fi>
2023-11-29 09:24:45 +0200

    feat(interim-feedback): create query function for getting interim feedback parent fbt

--- src/client/pages/FeedbackTarget/tabs/InterimFeedback/useInterimFeedbacks.js
+++ src/client/pages/FeedbackTarget/tabs/InterimFeedback/useInterimFeedbacks.js
@@ -3,6 +3,20 @@ import { useQuery } from 'react-query'
 import { INTERIM_FEEDBACKS_ENABLED } from '../../../../util/common'
 import apiClient from '../../../../util/apiClient'
 
+export const useInterimFeedbackParent = (interimFbtId, enable = true) => {
+  const queryFn = async () => {
+    const { data } = await apiClient.get(`/feedback-targets/interimFeedbacks/${interimFbtId}/parent`)
+
+    return data
+  }
+
+  const { data: parentFeedback, ...rest } = useQuery(['interimFeedbackParent', interimFbtId], queryFn, {
+    enabled: enable && INTERIM_FEEDBACKS_ENABLED,
+  })
+
+  return { parentFeedback, ...rest }
+}
+
 export const useInterimFeedbacks = (parentId, enable = true) => {
   const queryFn = async () => {
     const { data } = await apiClient.get(`/feedback-targets/${parentId}/interimFeedbacks`)
