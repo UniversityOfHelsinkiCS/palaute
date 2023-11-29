60573192c7ea1459393cd04eb25ae440eb26954f
Henri Remonen <henri@remonen.fi>
2023-11-29 14:04:44 +0200

    chore(interim-feedback): add translations

--- src/client/pages/MyTeaching/InterimFeedbackChip.js
+++ src/client/pages/MyTeaching/InterimFeedbackChip.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import styles from '../../util/chipStyles'
+import { useTranslation } from 'react-i18next'
+
 import LinkChip from '../../components/common/LinkChip'
 
+import styles from '../../util/chipStyles'
+
 const InterimFeedbackChip = ({ id }) => {
+  const { t } = useTranslation()
+
   const url = `/targets/${id}/interim-feedback`
 
   return (
     <LinkChip
       to={url}
-      label={'Välipalaute käynnissä'}
+      label={t('teacherView:interimFeedbackOpen')}
       sx={{
         ...styles.interim,
         ...styles.interactive,
