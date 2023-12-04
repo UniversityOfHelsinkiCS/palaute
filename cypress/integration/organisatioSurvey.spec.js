b726cb462a67597cab46adad6e97cc5e3eb44666
Henri Remonen <henri@remonen.fi>
2023-12-04 13:20:18 +0200

    test(org-survey): create tests for asserting the form is correctly rendered

--- cypress/integration/organisatioSurvey.spec.js
+++ cypress/integration/organisatioSurvey.spec.js
@@ -15,7 +15,7 @@ describe('Organisation Survey view', () => {
     cy.get('[data-cy="organisation-surveys-no-surveys-alert"]').should('be.visible')
   })
 
-  it('User with organisation access can create new organisation surveys', () => {
+  it('User with organisation can access new survey window and the form is rendered correctly', () => {
     cy.visit(`${baseUrl}/organisations/500-K005/organisation-surveys`)
 
     cy.get('[data-cy="organisation-surveys-add-new"]').should('be.visible').click()
@@ -23,9 +23,33 @@ describe('Organisation Survey view', () => {
     cy.get('[data-cy="organisation-surveys-add-new"]').should('be.disabled')
     cy.get('[data-cy="organisation-surveys-editor-title"]').should('be.visible')
 
-    cy.get('[data-cy="formik-locales-field-fi-name"]').should('be.visible').type('Testi kysely')
-    cy.get('[data-cy="formik-locales-field-sv-name"]').should('be.visible').type('Testundersökning')
-    cy.get('[data-cy="formik-locales-field-en-name"]').should('be.visible').type('Test survey')
+    cy.get('[data-cy="formik-locales-field-fi-name"]').should('be.visible')
+    cy.get('[data-cy="formik-locales-field-sv-name"]').should('be.visible')
+    cy.get('[data-cy="formik-locales-field-en-name"]').should('be.visible')
+
+    cy.get('[data-cy="formik-date-picker-field-startDate"]').should('be.visible')
+
+    cy.get('[data-cy="formik-date-picker-field-endDate"]').should('be.visible')
+
+    cy.get('[data-cy="formik-responsible-teacher-input-field"]').should('be.visible')
+
+    cy.get('[data-cy="formik-student-number-input-alert"]').should('be.visible')
+    cy.get('[data-cy="formik-student-number-input-expand-icon"]').should('be.visible')
+    cy.get('[data-cy="formik-student-number-input-delimeter-list"]').should('not.exist')
+    cy.get('[data-cy="formik-student-number-input-field"]').should('be.visible')
+
+    cy.get('[data-cy="formik-student-number-input-expand-icon"]').click()
+    cy.get('[data-cy="formik-student-number-input-delimeter-list"]').should('be.visible')
+  })
+
+  it('User with organisation access can fill in new organisation surveys', () => {
+    cy.visit(`${baseUrl}/organisations/500-K005/organisation-surveys`)
+
+    cy.get('[data-cy="organisation-surveys-add-new"]').click()
+
+    cy.get('[data-cy="formik-locales-field-fi-name"]').type('Testi kysely')
+    cy.get('[data-cy="formik-locales-field-sv-name"]').type('Testundersökning')
+    cy.get('[data-cy="formik-locales-field-en-name"]').type('Test survey')
 
     cy.get('[data-cy="formik-date-picker-field-startDate"]').should('be.visible')
 
