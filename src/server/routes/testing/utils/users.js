2a47aca206aa0a09e43af5409c3353b3a61beae9
Henri Remonen <henri@remonen.fi>
2023-12-05 13:49:52 +0200

    test: create user seeder for the database

---
+++ src/server/routes/testing/utils/users.js
@@ -0,0 +1,38 @@
+const testUsers = [
+  {
+    id: 'hy-hlo-1441871	',
+    username: 'mluukkai',
+    email: 'grp-toska@helsinki.fi',
+    studentNumber: '011120775',
+    employeeNumber: '9021313',
+    firstName: 'Matti',
+    lastName: 'Luukkainen',
+  },
+  {
+    id: 'hy-hlo-51367956	',
+    username: 'testiman',
+    email: 'Tommi.testaaja@toska.fi',
+    studentNumber: '123445678',
+    employeeNumber: '123445678',
+    firstName: 'Tommi',
+    lastName: 'Testaaja',
+  },
+  {
+    id: 'hy-hlo-115054920	',
+    username: 'oppilasolli',
+    email: 'opiskelija@toska.fi',
+    studentNumber: '115054920',
+    firstName: 'Olli',
+    lastName: 'Oppilas',
+  },
+  {
+    id: 'hy-hlo-1501077	',
+    username: 'danieldekaani',
+    email: 'dekaani@toska.fi',
+    employeeNumber: '1501077',
+    firstName: 'Daniel',
+    lastName: 'Dekaani',
+  },
+]
+
+module.exports = { testUsers }
