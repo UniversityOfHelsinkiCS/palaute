c3170f8376184c5017754eda73ea515810936296
Henri Remonen <henri@remonen.fi>
2023-12-20 10:48:07 +0200

    chore: allow for prop injection for LinkButton

--- src/client/components/common/LinkButton.js
+++ src/client/components/common/LinkButton.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import { Button, Link as MuiLink } from '@mui/material'
 import { OpenInNew } from '@mui/icons-material'
 
-const LinkButton = ({ title, to, external = false }) => {
+const LinkButton = ({ title, to, external = false, ...rest }) => {
   const buttonProps = {
     ...(external ? { component: MuiLink, href: to, endIcon: <OpenInNew /> } : { component: Link, to }),
   }
 
   return (
-    <Button size="small" {...buttonProps}>
+    <Button {...rest} size="small" {...buttonProps}>
       {title}
     </Button>
   )
