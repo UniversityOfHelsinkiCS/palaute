31c3d537c8d843d39a300a4aea7d0b27836adb42
aspahe <asta.hentila@leanware.fi>
2024-01-09 12:08:45 +0200

    no message

--- src/server/middleware/adminAccess.js
+++ src/server/middleware/adminAccess.js
@@ -1,6 +1,6 @@
 const { ApplicationError } = require('../util/customErrors')
 
-//testing
+// This middleware checks if the user is an admin and throws an error if not.
 const adminAccess = (req, _, next) => {
   if (!req.user.isAdmin) ApplicationError.Forbidden()
 
