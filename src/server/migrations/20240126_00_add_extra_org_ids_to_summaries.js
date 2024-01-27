ccd864a41b8dac804e8e534713a4d4a1f2e39a23
Veikkosuhonen <vesuvesuh@gmail.com>
2024-01-27 16:33:44 +0200

    fix: Delete old summaries in migration so they rebuild in cron

--- src/server/migrations/20240126_00_add_extra_org_ids_to_summaries.js
+++ src/server/migrations/20240126_00_add_extra_org_ids_to_summaries.js
@@ -1,8 +1,10 @@
 const { STRING, ARRAY } = require('sequelize')
+const { Summary } = require('../models')
 
 module.exports = {
   up: async queryInterface => {
     await queryInterface.removeIndex('summaries', 'summaries_entity_id_start_date_end_date')
+    await Summary.destroy({ where: {} })
     await queryInterface.addColumn('summaries', 'extra_org_ids', {
       type: ARRAY(STRING),
     })
