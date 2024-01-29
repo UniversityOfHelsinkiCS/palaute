fd0ab1f4071ae8de8d66d011fc6dc09ce69f3edf
Henri Remonen <henri@remonen.fi>
2024-01-29 08:42:47 +0200

    style(my-teaching-v2): update GroupAccordion styles

--- src/client/pages/MyTeaching/V2/GroupAccordion.js
+++ src/client/pages/MyTeaching/V2/GroupAccordion.js
@@ -1,18 +1,33 @@
 import React from 'react'
 
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Box, Divider } from '@mui/material'
+import { Typography, Box, Divider } from '@mui/material'
 import Masonry from '@mui/lab/Masonry'
 
 import { useTranslation } from 'react-i18next'
 
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-
 import CourseUnitAccordion from './CourseUnitAccordion'
 
 const styles = {
+  wrapper: {
+    marginTop: '4rem',
+    padding: '1rem',
+    borderTop: '1px solid #e0e0e0',
+    borderRight: '1px solid #e0e0e0',
+    borderLeft: '1px solid #e0e0e0',
+    position: 'relative',
+    '&:hover': {
+      borderColor: theme => theme.palette.primary.main,
+    },
+  },
   title: {
-    fontSize: '1.1rem',
+    marginTop: '-1.75em',
+    paddingX: '0.5em',
+    fontSize: '18px',
     fontWeight: theme => theme.typography.fontWeightMedium,
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: 'full',
+    zIndex: 1,
   },
   details: {
     flexDirection: 'column',
@@ -24,13 +39,12 @@ const GroupAccordion = ({ title, courseUnits, group }) => {
   const { t } = useTranslation()
 
   return (
-    <Accordion defaultExpanded sx={{ padding: 2 }}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography component="h2" variant="h5" sx={styles.title}>
-          Yliopistokurssit ({courseUnits.length})
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails sx={styles.details}>
+    <Box sx={styles.wrapper}>
+      <Typography component="h2" sx={styles.title}>
+        Yliopistokurssit ({courseUnits.length})
+      </Typography>
+
+      <Box sx={styles.details}>
         <Masonry spacing={2} columns={{ xs: 1, sm: 2, md: 3 }}>
           {courseUnits.map((courseUnit, i) => (
             <Box key={courseUnit.courseCode}>
@@ -39,8 +53,8 @@ const GroupAccordion = ({ title, courseUnits, group }) => {
             </Box>
           ))}
         </Masonry>
-      </AccordionDetails>
-    </Accordion>
+      </Box>
+    </Box>
   )
 }
 
