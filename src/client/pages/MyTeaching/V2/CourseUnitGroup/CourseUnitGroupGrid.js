c1ef6325f118e1548f4ed2485b5116757728f881
Henri Remonen <henri@remonen.fi>
2024-02-09 12:37:39 +0200

    refactor(my-teaching-v2): update the CourseUnitGroupGrid component

--- src/client/pages/MyTeaching/V2/CourseUnitGroup/CourseUnitGroupGrid.js
+++ src/client/pages/MyTeaching/V2/CourseUnitGroup/CourseUnitGroupGrid.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Grid } from '@mui/material'
 
 const CourseUnitGroupGrid = ({ children }) => (
-  <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
+  <Grid container spacing={2}>
     {children}
   </Grid>
 )
