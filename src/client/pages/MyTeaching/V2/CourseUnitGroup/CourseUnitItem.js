5c0e114dab6a2763ba0c997b4030e567eec34d3a
Henri Remonen <henri@remonen.fi>
2024-02-19 10:53:02 +0200

    feat(my-teaching-v2): indicate disabled course somehow

--- src/client/pages/MyTeaching/V2/CourseUnitGroup/CourseUnitItem.js
+++ src/client/pages/MyTeaching/V2/CourseUnitGroup/CourseUnitItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-
 import { Box, Typography } from '@mui/material'
 
 import FeedbackTargetList from '../FeedbackTargetList/FeedbackTargetList'
@@ -9,6 +8,7 @@ import InterimFeedbackChip from '../chips/InterimFeedbackChip'
 
 import { getLanguageValue } from '../../../../util/languageUtils'
 import { getCourseCode } from '../../../../util/courseIdentifiers'
+import DisabledCourseWarning from './DisabledCourseWarning'
 
 const styles = {
   item: {
@@ -19,6 +19,10 @@ const styles = {
     },
     minHeight: '100px',
   },
+  alert: {
+    backgroundColor: '#fddeaf',
+    border: '1px solid #6b3600',
+  },
   details: {
     display: 'block',
     padding: 0,
@@ -28,17 +32,18 @@ const styles = {
 const CourseUnitItem = ({ courseUnit }) => {
   const { i18n } = useTranslation()
 
-  const { name, courseRealisations } = courseUnit
+  const { name, courseRealisations, disabledCourse } = courseUnit
 
   const visibleCourseCode = getCourseCode(courseUnit)
   const courseName = getLanguageValue(name, i18n.language)
 
   return (
-    <Box sx={styles.item} data-cy="my-teaching-course-unit-item">
+    <Box sx={{ ...styles.item, ...(disabledCourse && styles.alert) }} data-cy="my-teaching-course-unit-item">
       <Box sx={{ px: 2, pt: 2 }}>
-        <Typography component="h3" variant="body1">
+        <Typography component="h3" variant="subtitle1" sx={{ fontWeight: 'bold' }}>
           {visibleCourseCode} {courseName}
         </Typography>
+        {disabledCourse && <DisabledCourseWarning />}
 
         {/* {interimFeedbackTargets.length > 0 && <InterimFeedbackChip parentFeedbackTarget={feedbackTarget} />} */}
       </Box>
