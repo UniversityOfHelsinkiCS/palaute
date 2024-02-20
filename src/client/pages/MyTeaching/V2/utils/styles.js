a654e8abb4a69dc09747f7781e4f81c8abf49251
Henri Remonen <henri@remonen.fi>
2024-02-20 09:51:50 +0200

    styles(my-teaching-v2): remove unused error style

--- src/client/pages/MyTeaching/V2/utils/styles.js
+++ src/client/pages/MyTeaching/V2/utils/styles.js
@@ -3,17 +3,6 @@ const commonStyles = {
     backgroundColor: '#fddeaf',
     border: '1px solid #6b3600',
     color: '#6b3600',
-    a: {
-      color: '#0f4480',
-    },
-  },
-  error: {
-    backgroundColor: '#ffb3b3',
-    border: '1px solid #6b0000',
-    color: '#6b0000',
-    a: {
-      color: '#0f4480',
-    },
   },
 }
 
