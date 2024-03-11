b658e2238914f34fcbd51677226dada3d53aea8c
Henri Remonen <henri@remonen.fi>
2024-03-11 12:17:07 +0200

    feat: fix organisation correspondents not showing to the admin users

--- src/server/routes/organisations/util.js
+++ src/server/routes/organisations/util.js
@@ -1,28 +1,13 @@
-const { Organisation } = require('../../models')
 const { ApplicationError } = require('../../util/customErrors')
 
-const getAdminAccess = async (user, code) => {
-  const organisation = await Organisation.findOne({ where: { code } })
-  return {
-    organisation,
-    hasReadAccess: true,
-    hasWriteAccess: true,
-    hasAdminAccess: true,
-  }
-}
-
 const getAccessAndOrganisation = async (user, code, requiredAccess) => {
-  if (user.isAdmin) {
-    return getAdminAccess(user, code)
-  }
-
   const organisationAccess = await user.getOrganisationAccess()
 
   const { access, organisation } = organisationAccess.find(({ organisation }) => organisation.code === code) ?? {}
 
-  const hasReadAccess = Boolean(access?.read)
-  const hasWriteAccess = Boolean(access?.write)
-  const hasAdminAccess = Boolean(access?.admin)
+  const hasReadAccess = user.isAdmin || Boolean(access?.read)
+  const hasWriteAccess = user.isAdmin || Boolean(access?.write)
+  const hasAdminAccess = user.isAdmin || Boolean(access?.admin)
 
   const missingRights = []
   if (requiredAccess?.read && !hasReadAccess) missingRights.push('read')
