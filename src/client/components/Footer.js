009080ca3fc15f2f6674c4c59df77b78df0d3cd9
Reija Laukko <reija.laukko@leanware.fi>
2024-03-21 17:26:16 +0200

    TAUKPJ-235 use lazy

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { lazy } from 'react'
 import { CUSTOM_FOOTER_COMPONENT } from '../util/common'
 
-const Footer = ({ user }) => {
-  const [FooterComponent, setFooterComponent] = useState(null)
-
-  useEffect(() => {
-    const loadComponent = async () => {
-      const componentName = CUSTOM_FOOTER_COMPONENT
-      if (!componentName) {
-        throw new Error('No footer component found')
-      }
-      const { default: Component } = await import(`./${componentName}`)
-      setFooterComponent(<Component user={user} />)
-    }
-
-    loadComponent()
-  }, [])
-
-  return FooterComponent
+const componentName = CUSTOM_FOOTER_COMPONENT
+if (!componentName) {
+  throw new Error('No footer component found')
 }
+const Component = lazy(() => import(`./${componentName}`))
+
+const Footer = ({ user }) => <Component user={user} />
 
 export default Footer
