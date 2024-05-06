aba5a12dfc9c86ebbaa5d9543db3cc69491e15f9
Henri Remonen <henri@remonen.fi>
2024-05-06 12:14:11 +0300

    fix: dynamic imports breaking Vite

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React, { lazy } from 'react'
 import { CUSTOM_FOOTER_COMPONENT } from '../util/common'
+import DefaultFooter from './Footer-default'
 
 const componentName = CUSTOM_FOOTER_COMPONENT
 if (!componentName) {
   throw new Error('No footer component found')
 }
-const Component = lazy(() => import(`./${componentName}`))
+// const Component = lazy(() => import(`./${componentName}.jsx`))
 
-const Footer = ({ user }) => <Component user={user} />
+const Footer = ({ user }) => <DefaultFooter user={user} />
 
 export default Footer
