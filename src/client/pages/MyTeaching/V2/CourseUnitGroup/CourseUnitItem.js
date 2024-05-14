0ac5f118eeaf31a7e06c053f3c7b196c0bd419af
Joni Sivula <joni.sivula@leanware.fi>
2024-05-14 20:58:33 +0300

    Adding chips back to course unit level

--- src/client/pages/MyTeaching/V2/CourseUnitGroup/CourseUnitItem.js
+++ src/client/pages/MyTeaching/V2/CourseUnitGroup/CourseUnitItem.js
@@ -9,6 +9,11 @@ import commonStyles from '../utils/styles'
 
 import { getLanguageValue } from '../../../../util/languageUtils'
 import { getCourseCode } from '../../../../util/courseIdentifiers'
+import { hasOngoingInterimFeedbacks } from '../utils/utils'
+import InterimFeedbackChip from '../chips/InterimFeedbackChip'
+import FeedbackResponseChip from '../../FeedbackResponseChip'
+import feedbackTargetIsOpen from '../../../../util/feedbackTargetIsOpen'
+import feedbackTargetCourseIsOngoing from '../../../../util/feedbackTargetCourseIsOngoing'
 
 const styles = {
   item: {
@@ -28,13 +33,52 @@ const CourseUnitItem = ({ courseUnit }) => {
   const visibleCourseCode = getCourseCode(courseUnit)
   const courseName = getLanguageValue(name, i18n.language)
 
+  let continuousFeedbackTargetFound = false
+  let ongoingFeedbackTargetFound = false
+  courseRealisations.forEach(cr => {
+    if (cr.feedbackTargets) {
+      for (const fbt of cr.feedbackTargets) {
+        const isOpen = feedbackTargetIsOpen(fbt)
+        const { opensAt } = fbt
+        const isOngoing = feedbackTargetCourseIsOngoing({ courseRealisation: cr, opensAt }) && !isOpen
+        if (isOngoing && fbt.continuousFeedbackEnabled) {
+          continuousFeedbackTargetFound = true
+        } else if (isOpen) {
+          ongoingFeedbackTargetFound = true
+        }
+      }
+    }
+  })
+
+  const fetchInterimFeedbackChip = courseRealisations.some(courseRealisation =>
+    hasOngoingInterimFeedbacks(courseRealisation.interimFeedbackTargets)
+  )
+
   return (
     <Box sx={{ ...styles.item }} data-cy={`my-teaching-course-unit-item-${visibleCourseCode || courseCode}`}>
-      <Box sx={{ px: 2, py: 2, ...(disabledCourse && commonStyles.alert) }}>
+      <Box sx={{ px: 2, paddingTop: 1, paddingBottom: 0, ...(disabledCourse && commonStyles.alert) }}>
         <Typography component="h3" variant="subtitle1" sx={{ fontWeight: 'bold' }}>
           {visibleCourseCode} {courseName}
         </Typography>
         {disabledCourse && <DisabledCourseWarning />}
+      </Box>
+
+      <Box sx={{ px: 2, py: 0, display: 'flex', flexWrap: 'wrap' }}>
+        <Box>
+          <Box sx={{ mr: 1 }}>{fetchInterimFeedbackChip && <InterimFeedbackChip />}</Box>
+        </Box>
+
+        <Box>
+          <Box>
+            {(ongoingFeedbackTargetFound || continuousFeedbackTargetFound) && (
+              <FeedbackResponseChip
+                id={undefined}
+                ongoing={ongoingFeedbackTargetFound}
+                continuous={continuousFeedbackTargetFound}
+              />
+            )}
+          </Box>
+        </Box>
       </Box>
 
       <Box sx={styles.details}>
