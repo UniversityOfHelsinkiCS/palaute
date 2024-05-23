2cefac2abc6080b69201f3e1049d8e9ad23a6912
Henri Remonen <henri@remonen.fi>
2024-05-23 13:24:44 +0300

    feat: accessibility betterments for tab group -refs: #1289

--- src/client/components/common/TabGroup.jsx
+++ src/client/components/common/TabGroup.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 import { Box, Divider, Paper, Typography } from '@mui/material'
 
 export const TabGroup = ({ title, hideTitle = false, Icon, children }) => (
-  <Box display="flex">
-    <Box display="flex" flexDirection="column" pt="0.6rem">
-      {!hideTitle && (
-        <Box display="flex" gap="0.5rem" px="1.5rem" mb="auto" color="textSecondary" alignItems="center">
-          {Icon && <Icon sx={theme => ({ color: theme.palette.text.secondary })} />}
-          <Typography variant="button" color="textSecondary" sx={{ userSelect: 'none' }} fontSize="small">
-            {title}
-          </Typography>
-        </Box>
-      )}
-      <Box display="flex" alignItems="end">
-        {children}
+  <Box display="flex" flexDirection="column" pt="0.6rem">
+    {!hideTitle && (
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '0.5rem',
+          px: '1.5rem',
+          my: 'auto',
+          alignItems: 'center',
+        }}
+      >
+        {Icon && <Icon aria-hidden="true" sx={theme => ({ color: theme.palette.text.secondary })} />}
+        <Typography
+          id={`tab-list-${title}`}
+          variant="button"
+          color="textSecondary"
+          sx={{ userSelect: 'none' }}
+          fontSize="small"
+        >
+          {title}
+        </Typography>
       </Box>
+    )}
+    <Box role="tablist" aria-labelledby={`tab-list-${title}`} sx={{ display: 'flex', alignItems: 'end' }}>
+      {children}
     </Box>
   </Box>
 )
