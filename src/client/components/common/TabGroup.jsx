f11285c06b447e45f6cfde4c098f5bc0334beaf6
Henri Remonen <henri@remonen.fi>
2024-05-24 12:05:38 +0300

    refactor: restore origin TabGroup

--- src/client/components/common/TabGroup.jsx
+++ src/client/components/common/TabGroup.jsx
@@ -1,60 +1,52 @@
 import React from 'react'
-import { Box, Divider, Paper, Tabs, Typography } from '@mui/material'
+import { Box, Divider, Paper, Typography } from '@mui/material'
 
 export const TabGroup = ({ title, hideTitle = false, Icon, children }) => (
-  <Box display="flex" flexDirection="column" pt="0.6rem">
-    {!hideTitle && (
-      <Box
-        role="region"
-        aria-labelledby={`tab-list-${title}`}
-        sx={{
-          display: 'flex',
-          gap: '0.5rem',
-          px: '1.5rem',
-          my: 'auto',
-          alignItems: 'center',
-        }}
-      >
-        {Icon && <Icon aria-hidden="true" sx={theme => ({ color: theme.palette.text.secondary })} />}
-        <Typography
-          id={`tab-list-${title}`}
-          variant="button"
-          color="textSecondary"
-          sx={{ userSelect: 'none' }}
-          fontSize="small"
-        >
-          {title}
-        </Typography>
+  <Box display="flex">
+    <Box display="flex" flexDirection="column" pt="0.6rem">
+      {!hideTitle && (
+        <Box display="flex" gap="0.5rem" px="1.5rem" mb="auto" color="textSecondary" alignItems="center">
+          {Icon && <Icon sx={theme => ({ color: theme.palette.text.secondary })} />}
+          <Typography variant="button" color="textSecondary" sx={{ userSelect: 'none' }} fontSize="small">
+            {title}
+          </Typography>
+        </Box>
+      )}
+      <Box display="flex" alignItems="end">
+        {children}
       </Box>
-    )}
-    <Box display="flex" alignItems="end">
-      {children}
     </Box>
   </Box>
 )
 
-export const TabGroupsContainer = ({ children }) => (
-  <Paper>
-    <Tabs
-      variant="scrollable"
-      scrollButtons="auto"
-      value={false}
-      sx={{
-        my: 3,
-        '& .MuiTabs-indicator': {
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: 'transparent',
-        },
-        '& .MuiTabs-indicatorSpan': {
-          maxWidth: 80,
-          width: '100%',
-          backgroundColor: theme => theme.palette.primary.main,
-        },
-      }}
-      TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
-    >
-      {children}
-    </Tabs>
-  </Paper>
-)
+export const TabGroupsContainer = ({ children }) => {
+  const childElements = children.filter(child => Boolean(child))
+
+  return (
+    <Paper>
+      <Box
+        display="flex"
+        px="0.2rem"
+        alignItems="stretch"
+        sx={{
+          overflowX: 'auto',
+          '::-webkit-scrollbar': {
+            display: 'none',
+          },
+          borderRadius: '0.8rem',
+        }}
+      >
+        {React.Children.map(childElements, (child, i) => {
+          const lastChild = childElements.length === i + 1
+
+          return (
+            <>
+              {child}
+              {lastChild ? null : <Divider orientation="vertical" flexItem />}
+            </>
+          )
+        })}
+      </Box>
+    </Paper>
+  )
+}
