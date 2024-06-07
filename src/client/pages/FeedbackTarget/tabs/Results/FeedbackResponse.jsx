9948b33637e4693fb125d9da8e45718f56406d8d
Henri Remonen <henri@remonen.fi>
2024-06-07 09:38:59 +0300

    style: more consisten feedback response style with the other sections

--- src/client/pages/FeedbackTarget/tabs/Results/FeedbackResponse.jsx
+++ src/client/pages/FeedbackTarget/tabs/Results/FeedbackResponse.jsx
@@ -8,19 +8,6 @@ import { useFeedbackTargetContext } from '../../FeedbackTargetContext'
 import Markdown from '../../../../components/common/Markdown'
 import { boxPrintStyle } from '../../../../util/printStyle'
 
-const styles = {
-  title: {
-    fontWeight: 'semibold',
-    fontSize: '1.5rem',
-    '@media print': { fontSize: '1.2rem' },
-    mb: '1rem',
-    px: '1rem',
-  },
-  responseBox: {
-    p: '1rem',
-  },
-}
-
 const FeedbackResponse = ({ feedbackTarget }) => {
   const { t } = useTranslation()
 
@@ -31,37 +18,46 @@ const FeedbackResponse = ({ feedbackTarget }) => {
   const canGiveFeedbackResponse = isEnded && isResponsibleTeacher
 
   return (
-    <Card sx={{ mb: '4rem', borderRadius: '1rem', ...boxPrintStyle }}>
-      <CardContent>
-        <Typography component="h2" sx={styles.title}>
-          {t('feedbackTargetResults:responseHeading')}
-        </Typography>
-
-        {!feedbackResponse && <Alert severity="info">{t('feedbackTargetResults:noResponseInfo')}</Alert>}
-        {feedbackResponse && (
-          <Box sx={styles.responseBox}>
-            <Markdown>{feedbackResponse}</Markdown>
-          </Box>
-        )}
-
-        {isResponsibleTeacher && !feedbackResponse && (
-          <Tooltip title={t('feedbackTargetResults:giveResponseInfo')}>
-            <Box mt={2} width="max-content">
-              <Button
-                disabled={!canGiveFeedbackResponse}
-                variant="contained"
-                color="primary"
-                component={Link}
-                to={`/targets/${id}/edit-feedback-response`}
-                sx={{ '@media print': { display: 'none' } }}
-              >
-                {t('feedbackTargetResults:giveResponse')}
-              </Button>
+    <>
+      <Typography
+        component="h2"
+        sx={{
+          fontWeight: 'semibold',
+          fontSize: '1.5rem',
+          '@media print': { fontSize: '1.2rem' },
+          mb: '2rem',
+        }}
+      >
+        {t('feedbackTargetResults:responseHeading')}
+      </Typography>
+      <Card sx={{ mb: '4rem', borderRadius: '1rem', ...boxPrintStyle }}>
+        <CardContent>
+          {!feedbackResponse && <Alert severity="info">{t('feedbackTargetResults:noResponseInfo')}</Alert>}
+          {feedbackResponse && (
+            <Box sx={{ p: '1rem' }}>
+              <Markdown>{feedbackResponse}</Markdown>
             </Box>
-          </Tooltip>
-        )}
-      </CardContent>
-    </Card>
+          )}
+
+          {isResponsibleTeacher && !feedbackResponse && (
+            <Tooltip title={t('feedbackTargetResults:giveResponseInfo')}>
+              <Box mt={2} width="max-content">
+                <Button
+                  disabled={!canGiveFeedbackResponse}
+                  variant="contained"
+                  color="primary"
+                  component={Link}
+                  to={`/targets/${id}/edit-feedback-response`}
+                  sx={{ '@media print': { display: 'none' } }}
+                >
+                  {t('feedbackTargetResults:giveResponse')}
+                </Button>
+              </Box>
+            </Tooltip>
+          )}
+        </CardContent>
+      </Card>
+    </>
   )
 }
 
