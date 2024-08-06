4276c6bcdd172d69c069a57441e388917dc4d81f
Joni Sivula <joni.sivula@leanware.fi>
2024-08-06 18:43:28 +0300

    Fixed default value of not giving feedback column

--- src/server/migrations/20240704_01_add_not_giving_feedback_to_user_feedback_targets.js
+++ src/server/migrations/20240704_01_add_not_giving_feedback_to_user_feedback_targets.js
@@ -2,7 +2,11 @@ const { BOOLEAN } = require('sequelize')
 
 module.exports = {
   up: async queryInterface => {
-    await queryInterface.addColumn('user_feedback_targets', 'not_giving_feedback', { type: BOOLEAN })
+    await queryInterface.addColumn('user_feedback_targets', 'not_giving_feedback', {
+      type: BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    })
   },
   down: async queryInterface => {
     await queryInterface.removeColumn('user_feedback_targets', 'not_giving_feedback')
