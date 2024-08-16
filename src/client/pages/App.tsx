e7bcf1a7bb93fce5e452a95012c5b05e7941ed08
Henri Remonen <henri@remonen.fi>
2024-08-16 09:24:52 +0300

    feat: try out shibboleth pinger

--- src/client/pages/App.tsx
+++ src/client/pages/App.tsx
@@ -1,17 +1,17 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
-
 import { useTranslation } from 'react-i18next'
+import { initShibbolethPinger } from 'unfuck-spa-shibboleth-session'
+
 import LocalizationProvider from '../components/LocalizationProvider'
 import CustomUiConfigProvider from '../components/CustomUiConfigProvider'
 import AdUser from './AdUser'
 import GuestUser from './GuestUser'
 import useTheme from '../theme'
-import usePinger from '../hooks/usePinger'
-import { inE2EMode, UI_CONFIG_NAME } from '../util/common'
+import { UI_CONFIG_NAME } from '../util/common'
 import useCustomUiConfig from '../hooks/useCustomUiConfig'
 
 const App = () => {
@@ -20,7 +20,9 @@ const App = () => {
   const customUiConfig: any = useCustomUiConfig(UI_CONFIG_NAME)
   const theme = useTheme(customUiConfig?.theme)
 
-  usePinger({ enabled: !inE2EMode })
+  useEffect(() => {
+    initShibbolethPinger()
+  }, [])
 
   // Change the document language according to the i18n language
   i18n.on('languageChanged', (lng: 'en' | 'fi' | 'sv') => {
