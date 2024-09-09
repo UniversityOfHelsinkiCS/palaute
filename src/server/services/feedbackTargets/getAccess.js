d4df64fb70d504437c6e8d1679e2f03f17790cd4
Reija Laukko <reija.laukko@leanware.fi>
2024-09-09 15:45:53 +0300

    User has both direct access and access through organisation, try to choose highest

--- src/server/services/feedbackTargets/getAccess.js
+++ src/server/services/feedbackTargets/getAccess.js
@@ -4,23 +4,44 @@ const getAccess = async ({ userFeedbackTarget, user, feedbackTarget }) => {
   if (user.dataValues.isAdmin) return Access.ADMIN
 
   const accessStatus = userFeedbackTarget?.accessStatus
-
+  let uftAccess = null
   if (accessStatus) {
-    return Access.For(accessStatus)
+    uftAccess = Access.For(accessStatus)
   }
 
   // User not directly associated. Lets check if they have access through organisation
   const organisationAccess = await user.getOrganisationAccessByCourseUnitId(feedbackTarget.courseUnitId)
 
-  if (!organisationAccess) {
-    return null
-  }
-
+  let orgAccess = Access.ORGANISATION_READ
   if (organisationAccess.admin) {
-    return Access.ORGANISATION_ADMIN
+    orgAccess = Access.ORGANISATION_ADMIN
   }
 
-  return Access.ORGANISATION_READ
+  // only direct access, return that
+  if (uftAccess !== null && orgAccess === null) {
+    return uftAccess
+  }
+  // access only through organisation, return that
+  if (uftAccess === null && orgAccess !== null) {
+    return orgAccess
+  }
+  // both direct access and access through organisation, return highest
+  if (uftAccess !== null && orgAccess !== null) {
+    if (orgAccess === Access.ORGANISATION_ADMIN) {
+      if (uftAccess === Access.ADMIN || uftAccess === Access.RESPONSIBLE_TEACHER) {
+        return uftAccess
+      }
+      return orgAccess
+    }
+    if (orgAccess === Access.ORGANISATION_READ) {
+      if (uftAccess === Access.STUDENT) {
+        return orgAccess
+      }
+      return uftAccess
+    }
+  }
+
+  return null
 }
 
 module.exports = { getAccess }
