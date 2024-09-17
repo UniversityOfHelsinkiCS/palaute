4728e2416ab51d05b9d76db95853b26a75cf7660
Henri Remonen <henri@remonen.fi>
2024-09-17 15:13:26 +0300

    chore: remove clog

--- src/client/hooks/pinger/Pinger-shibboleth.tsx
+++ src/client/hooks/pinger/Pinger-shibboleth.tsx
@@ -8,8 +8,6 @@ const useShibbolethPinger = () => {
   if (inProduction || inStaging) {
     useEffect(() => {
       initShibbolethPinger()
-
-      console.log('Shibboleth pinger initialized')
     }, [])
   }
 }
