7a3675097b435e8d19e715faa547ba2d8b907433
Henri Remonen <henri@remonen.fi>
2024-09-17 10:39:56 +0300

    chore: use shibboleth pinger on stating as well

--- src/client/hooks/pinger/Pinger-shibboleth.tsx
+++ src/client/hooks/pinger/Pinger-shibboleth.tsx
@@ -2,10 +2,10 @@ import { useEffect } from 'react'
 // @ts-expect-error Because the package is not typed
 import { initShibbolethPinger } from 'unfuck-spa-shibboleth-session'
 
-import { inProduction } from '../../util/common'
+import { inProduction, inStaging } from '../../util/common'
 
 const useShibbolethPinger = () => {
-  if (inProduction) {
+  if (inProduction || inStaging) {
     useEffect(() => {
       initShibbolethPinger()
     }, [])
