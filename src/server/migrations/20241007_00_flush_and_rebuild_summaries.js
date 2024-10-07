f7bb74ebd35205e6d472f01832088fd2dd64b108
Henri Remonen <henri@remonen.fi>
2024-10-07 09:29:49 +0300

    feat: new migration for flushing the summaries table

---
+++ src/server/migrations/20241007_00_flush_and_rebuild_summaries.js
@@ -0,0 +1,11 @@
+const { buildSummaries } = require('services/summary/buildSummaries')
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.sequelize.query('TRUNCATE summaries;')
+    await queryInterface.sequelize.query('DELETE FROM summaries;')
+
+    await buildSummaries()
+  },
+  down: async queryInterface => {},
+}
