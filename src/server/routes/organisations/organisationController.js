97fc385a6a5bbf90f1f4f94dcb965162d812ec32
tfhuhtal <tuomas.huhtala@icloud.com>
2024-11-05 14:07:32 +0200

    adding start and end dates to query

--- src/server/routes/organisations/organisationController.js
+++ src/server/routes/organisations/organisationController.js
@@ -1,11 +1,12 @@
-const _ = require('lodash');
-const { Router } = require('express');
-const { Op } = require('sequelize');
+const _ = require('lodash')
+const { Router } = require('express')
+const { Op } = require('sequelize')
 
+const { formatActivityPeriod } = require('util/common')
 const {
   getOrganisationsList,
-} = require('../../services/organisations/getOrganisationsList');
-const { ORGANISATION_SURVEYS_ENABLED } = require('../../util/config');
+} = require('../../services/organisations/getOrganisationsList')
+const { ORGANISATION_SURVEYS_ENABLED } = require('../../util/config')
 const {
   Organisation,
   OrganisationLog,
@@ -14,65 +15,65 @@ const {
   CourseRealisation,
   CourseUnitsOrganisation,
   FeedbackTarget,
-} = require('../../models');
-const { ApplicationError } = require('../../util/customErrors');
-const { createOrganisationLog } = require('../../services/auditLog');
-const getOpenFeedbackByOrganisation = require('./getOpenFeedbackByOrganisation');
-const { getAccessAndOrganisation } = require('./util');
-const feedbackCorrespondentRouter = require('./feedbackCorrespondentController');
-const organisationSurveyRouter = require('./organisationSurveyController');
+} = require('../../models')
+const { ApplicationError } = require('../../util/customErrors')
+const { createOrganisationLog } = require('../../services/auditLog')
+const getOpenFeedbackByOrganisation = require('./getOpenFeedbackByOrganisation')
+const { getAccessAndOrganisation } = require('./util')
+const feedbackCorrespondentRouter = require('./feedbackCorrespondentController')
+const organisationSurveyRouter = require('./organisationSurveyController')
 const {
   getOrganisationData: getOrganisationDataFromJami,
-} = require('../../util/jami');
+} = require('../../util/jami')
 
 const getUpdatedCourseCodes = async (updatedCourseCodes, organisation) => {
-  const organisationCourseCodes = await organisation.getCourseCodes();
+  const organisationCourseCodes = await organisation.getCourseCodes()
 
   return _.uniq(
     updatedCourseCodes.filter((c) => organisationCourseCodes.includes(c))
-  );
-};
+  )
+}
 
 const getOrganisations = async (req, res) => {
-  const { user, headers, isAdmin } = req;
+  const { user, headers, isAdmin } = req
 
-  const isEmployee = Boolean(headers?.employeenumber);
+  const isEmployee = Boolean(headers?.employeenumber)
 
   if (!isAdmin && !isEmployee) {
-    return res.send([]);
+    return res.send([])
   }
 
-  const organisationAccess = await user.getOrganisationAccess();
+  const organisationAccess = await user.getOrganisationAccess()
 
   const organisations = organisationAccess.map(({ organisation, access }) => ({
     ...organisation.toJSON(),
     access,
-  }));
+  }))
 
-  return res.send(organisations);
-};
+  return res.send(organisations)
+}
 
 const getOrganisationData = async (req, res) => {
-  const data = await getOrganisationDataFromJami();
+  const data = await getOrganisationDataFromJami()
 
-  return res.send(data);
-};
+  return res.send(data)
+}
 
 const getOrganisationsListHandler = async (req, res) => {
-  const organisationsList = await getOrganisationsList();
+  const organisationsList = await getOrganisationsList()
 
-  return res.send(organisationsList);
-};
+  return res.send(organisationsList)
+}
 
 const updateOrganisation = async (req, res) => {
-  const { user, body } = req;
-  const { code } = req.params;
+  const { user, body } = req
+  const { code } = req.params
 
   const { organisation, hasAdminAccess } = await getAccessAndOrganisation(
     user,
     code,
     { write: true }
-  );
+  )
 
   const updates = _.pick(body, [
     'studentListVisible',
@@ -80,7 +81,7 @@ const updateOrganisation = async (req, res) => {
     'disabledCourseCodes',
     'studentListVisibleCourseCodes',
     'publicQuestionIds',
-  ]);
+  ])
 
   if (
     !hasAdminAccess &&
@@ -89,40 +90,40 @@ const updateOrganisation = async (req, res) => {
     throw new ApplicationError(
       403,
       'Course codes can only be updated by organisation admins'
-    );
+    )
   }
 
   if (updates.disabledCourseCodes) {
     updates.disabledCourseCodes = await getUpdatedCourseCodes(
       updates.disabledCourseCodes,
       organisation
-    );
+    )
   }
 
   if (updates.studentListVisibleCourseCodes) {
     updates.studentListVisibleCourseCodes = await getUpdatedCourseCodes(
       updates.studentListVisibleCourseCodes,
       organisation
-    );
+    )
   }
 
-  await createOrganisationLog(organisation, updates, user);
+  await createOrganisationLog(organisation, updates, user)
 
-  Object.assign(organisation, updates);
+  Object.assign(organisation, updates)
 
-  const updatedOrganisation = await organisation.save();
+  const updatedOrganisation = await organisation.save()
 
-  return res.send(updatedOrganisation);
-};
+  return res.send(updatedOrganisation)
+}
 
 const getOrganisationByCode = async (req, res) => {
-  const { user } = req;
-  const { code } = req.params;
+  const { user } = req
+  const { code } = req.params
 
   const { organisation, hasReadAccess, hasWriteAccess, hasAdminAccess } =
     await getAccessAndOrganisation(user, code, {
       read: true,
-    });
+    })
 
   const theOrganisation = await Organisation.findOne({
     where: {
@@ -135,12 +136,9 @@ const getOrganisationByCode = async (req, res) => {
         attributes: ['id', 'firstName', 'lastName', 'email'],
       },
     ],
-  });
+  })
 
-  const tags = _.orderBy(
-    await theOrganisation.getTags(),
-    (tag) => tag.name?.fi
-  );
+  const tags = _.orderBy(await theOrganisation.getTags(), (tag) => tag.name?.fi)
 
   const publicOrganisation = {
     ...organisation.toJSON(),
@@ -151,17 +149,17 @@ const getOrganisationByCode = async (req, res) => {
       write: hasWriteAccess,
       admin: hasAdminAccess,
     },
-  };
+  }
 
-  return res.send(publicOrganisation);
-};
+  return res.send(publicOrganisation)
+}
 
 const getOrganisationLogs = async (req, res) => {
-  const { user } = req;
-  const { code } = req.params;
+  const { user } = req
+  const { code } = req.params
 
   if (!user.isAdmin) {
-    throw new ApplicationError('Forbidden', 403);
+    throw new ApplicationError('Forbidden', 403)
   }
 
   const { organisationLogs } = await Organisation.findOne({
@@ -181,46 +179,47 @@ const getOrganisationLogs = async (req, res) => {
         as: 'user',
       },
     },
-  });
+  })
 
-  return res.send(organisationLogs);
-};
+  return res.send(organisationLogs)
+}
 
 const getOpenQuestionsByOrganisation = async (req, res) => {
-  const { user } = req;
-  const { code } = req.params;
+  const { user } = req
+  const { code } = req.params
 
-  const organisationAccess = await user.getOrganisationAccess();
+  const organisationAccess = await user.getOrganisationAccess()
 
   const access = organisationAccess.filter(
     (org) => org.organisation.code === code
-  );
+  )
 
   if (access.length === 0) {
-    throw new ApplicationError('Forbidden', 403);
+    throw new ApplicationError('Forbidden', 403)
   }
 
-  const codesWithIds = await getOpenFeedbackByOrganisation(code);
+  const codesWithIds = await getOpenFeedbackByOrganisation(code)
 
-  return res.send(codesWithIds);
-};
+  return res.send(codesWithIds)
+}
 
 const findFeedbackTargets = async (req, res) => {
-  const {
-    user,
-    query: { courseCode, name },
-  } = req;
-  const { code } = req.params;
+  const { user, query } = req
+  const { code } = req.params
 
-  const organisationAccess = await user.getOrganisationAccess();
+  const organisationAccess = await user.getOrganisationAccess()
+
+  const { filter } = query
 
   const access = organisationAccess.filter(
     (org) => org.organisation.code === code
-  );
+  )
 
   if (access.length === 0) {
-    throw new ApplicationError('Forbidden', 403);
+    throw new ApplicationError('Forbidden', 403)
   }
+
+  const activityPeriod = formatActivityPeriod(query)
 
   const courseUnits = await CourseUnit.findAll({
     include: [
@@ -234,7 +233,37 @@ const findFeedbackTargets = async (req, res) => {
             model: CourseRealisation,
             as: 'courseRealisation',
             attributes: ['id', 'name', 'startDate', 'endDate'],
-            where: { userCreated: false },
+            where: {
+              ...(activityPeriod?.startDate &&
+                activityPeriod?.endDate && {
+                  [Op.or]: [
+                    {
+                      startDate: {
+                        [Op.between]: [
+                          activityPeriod.startDate,
+                          activityPeriod.endDate,
+                        ],
+                      },
+                    },
+                    {
+                      endDate: {
+                        [Op.between]: [
+                          activityPeriod.startDate,
+                          activityPeriod.endDate,
+                        ],
+                      },
+                    },
+                    {
+                      startDate: {
+                        [Op.lte]: activityPeriod.startDate,
+                      },
+                      endDate: {
+                        [Op.gte]: activityPeriod.endDate,
+                      },
+                    },
+                  ],
+                }),
+            },
           },
         ],
       },
@@ -249,30 +278,30 @@ const findFeedbackTargets = async (req, res) => {
     attributes: ['id', 'name', 'courseCode'],
     where: {
       [Op.or]: {
-        courseCode: { [Op.iLike]: `${courseCode}%` },
+        courseCode: { [Op.iLike]: `${filter}%` },
         [Op.or]: [
-          { 'name.fi': { [Op.iLike]: `%${name}%` } },
-          { 'name.se': { [Op.iLike]: `%${name}%` } },
-          { 'name.en': { [Op.iLike]: `%${name}%` } },
+          { 'name.fi': { [Op.iLike]: `%${filter}%` } },
+          { 'name.se': { [Op.iLike]: `%${filter}%` } },
+          { 'name.en': { [Op.iLike]: `%${filter}%` } },
         ],
       },
     },
-  });
+  })
 
-  res.send(courseUnits);
-};
+  res.send(courseUnits)
+}
 
-const router = Router();
+const router = Router()
 
-router.get('/', getOrganisations);
-router.get('/data', getOrganisationData);
-router.get('/list', getOrganisationsListHandler);
-router.put('/:code', updateOrganisation);
-router.get('/:code', getOrganisationByCode);
-router.get('/:code/open', getOpenQuestionsByOrganisation);
-router.get('/:code/logs', getOrganisationLogs);
-router.get('/:code/courses', findFeedbackTargets);
-if (ORGANISATION_SURVEYS_ENABLED) router.use('/', organisationSurveyRouter);
-router.use('/', feedbackCorrespondentRouter);
+router.get('/', getOrganisations)
+router.get('/data', getOrganisationData)
+router.get('/list', getOrganisationsListHandler)
+router.put('/:code', updateOrganisation)
+router.get('/:code', getOrganisationByCode)
+router.get('/:code/open', getOpenQuestionsByOrganisation)
+router.get('/:code/logs', getOrganisationLogs)
+router.get('/:code/courses', findFeedbackTargets)
+if (ORGANISATION_SURVEYS_ENABLED) router.use('/', organisationSurveyRouter)
+router.use('/', feedbackCorrespondentRouter)
 
-module.exports = router;
+module.exports = router
