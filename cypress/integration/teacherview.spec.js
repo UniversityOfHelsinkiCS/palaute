33e0f6429cd47d0537929335af6a9274b957621b
Henri Remonen <henri@remonen.fi>
2024-12-17 09:31:18 +0200

    test: fix test case -refs: #1418

--- cypress/integration/teacherview.spec.js
+++ cypress/integration/teacherview.spec.js
@@ -96,7 +96,7 @@ describe('Teacher view', () => {
     cy.get('@badge').contains('1').should('exist')
 
     cy.get('[data-cy=my-teaching-ended-tab]').trigger('mouseover')
-    cy.contains('Ended surveys: 1 missing counter feedbacks from the last academic year').should('be.visible')
+    cy.contains('Ended: 1 missing counter feedbacks from the last academic year').should('be.visible')
 
     // Check that the counter feedback missing chip is rendered on the CU level
     cy.get('[data-cy=my-teaching-course-unit-accordion-TEST_COURSE]').should('exist')
