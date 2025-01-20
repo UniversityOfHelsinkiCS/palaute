c2e85758a162c9eb4edfe4bb86d120ad6898e3d4
Veikkosuhonen <vesuvesuh@gmail.com>
2025-01-20 12:18:13 +0200

    fix: Migration has to delete most existing summaries

--- src/server/migrations/20250113_00_add_unique_index_summaries_feedback_target_id.js
+++ src/server/migrations/20250113_00_add_unique_index_summaries_feedback_target_id.js
@@ -1,5 +1,6 @@
 module.exports = {
   up: async queryInterface => {
+    await queryInterface.sequelize.query('DELETE FROM summaries WHERE feedback_target_id IS NOT NULL')
     await queryInterface.addIndex('summaries', {
       unique: true,
       fields: ['feedback_target_id'],
