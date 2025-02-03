a151d406de6d51263dbfde85f5b19aee7d6a5787
Veikkosuhonen <vesuvesuh@gmail.com>
2025-02-03 17:47:24 +0200

    chore: Remove unused test endpoints

--- src/server/test/testingController.js
+++ src/server/test/testingController.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const _ = require('lodash')
 const morgan = require('morgan')
 
-const { FeedbackTarget, CourseRealisation, User } = require('../models')
+const { FeedbackTarget } = require('../models')
 
 const { ApplicationError } = require('../util/customErrors')
 const { initTestSummary } = require('./seedSummary')
@@ -11,79 +11,6 @@ const { seedDb, seedUsers, seedOrganisationCorrespondent } = require('./seed')
 const { TEST_COURSE_REALISATION_ID } = require('./testIds')
 const { inProduction } = require('../util/config')
 const { getUniversitySurvey } = require('../services/surveys')
-
-const updateCourseRealisation = async (req, res) => {
-  const { feedbackTargetId } = req.params
-
-  const feedbackTarget = await FeedbackTarget.findByPk(Number(feedbackTargetId))
-
-  if (!feedbackTarget) ApplicationError.NotFound(`FBT ${feedbackTargetId} not found`)
-
-  const courseRealisation = await CourseRealisation.findByPk(feedbackTarget.courseRealisationId)
-
-  if (!courseRealisation) ApplicationError.NotFound(`Course realisation ${courseRealisation.id} not found`)
-
-  const updates = _.pick(req.body, ['startDate', 'endDate'])
-
-  Object.assign(courseRealisation, updates)
-
-  await courseRealisation.save()
-
-  const { feedbackResponse, feedbackResponseEmailSent } = req.body
-
-  Object.assign(feedbackTarget, {
-    opensAt: updates.startDate,
-    closesAt: updates.endDate,
-    feedbackResponse,
-    feedbackResponseEmailSent,
-  })
-
-  await feedbackTarget.save()
-
-  return res.sendStatus(200)
-}
-
-const updateManyCourseRealisations = async (req, res) => {
-  const { user } = req
-
-  if (!user) throw new ApplicationError('No user found', 404)
-
-  const { feedbackTargetIds } = _.pick(req.body, ['feedbackTargetIds'])
-
-  const updates = _.pick(req.body, ['startDate', 'endDate'])
-
-  /* eslint-disable */
-  for (const id of feedbackTargetIds) {
-    const feedbackTarget = await FeedbackTarget.findByPk(Number(id))
-    if (!feedbackTarget) ApplicationError.NotFound(`FBT ${id} not found`)
-
-    const courseRealisation = await CourseRealisation.findByPk(feedbackTarget.courseRealisationId)
-    if (!courseRealisation) ApplicationError.NotFound(`CUR ${feedbackTarget.courseRealisationId} not found`)
-
-    Object.assign(courseRealisation, updates)
-
-    await courseRealisation.save()
-  }
-  /* eslint-enable */
-
-  return res.send(200)
-}
-
-const updateUser = async (req, res) => {
-  const { userId } = req.params
-
-  const user = await User.findByPk(userId)
-
-  if (!user) throw new ApplicationError('User not found', 404)
-
-  const updates = _.pick(req.body, ['employeeNumber', 'studentNumber', 'username'])
-
-  Object.assign(user, updates)
-
-  await user.save()
-
-  return res.send(200)
-}
 
 const initSummary = async (req, res) => {
   await initTestSummary({ user: _.pick(req.body, ['hyPersonSisuId', 'uid']) })
@@ -156,11 +83,6 @@ router.use((_, __, next) => {
 
 router.use(Router.json())
 router.use(morgan('dev'))
-
-router.put('/user/:userId', updateUser)
-
-router.put('/courseRealisation/:feedbackTargetId', updateCourseRealisation)
-router.put('/courseRealisations', updateManyCourseRealisations)
 
 router.post('/init-summary', initSummary)
 router.post('/seed-users', seedTestUsers2)
