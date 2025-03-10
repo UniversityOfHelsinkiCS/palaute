ff641a9bb08498175c2eb11d2853508ac65aba73
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2025-03-10 14:35:44 +0200

    fix: Cypress configs

---
+++ cypress.config.ci.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1800,
+  viewportHeight: 1200,
+  defaultCommandTimeout: 10000,
+  video: true,
+  e2e: {
+    baseUrl: 'http://localhost:8000',
+    supportFile: 'cypress/support/index.js',
+    specPattern: 'cypress/**/*.spec.js',
+    experimentalRunAllSpecs: true,
+  },
+})
