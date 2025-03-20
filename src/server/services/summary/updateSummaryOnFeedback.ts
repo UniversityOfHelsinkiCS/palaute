c7570c65121f9e9a731e0345b0c4a437c20cff30
Veikkosuhonen <vesuvesuh@gmail.com>
2025-03-20 16:04:07 +0200

    fix: Purkkafix studentCount to be updated whenever feedback given

--- src/server/services/summary/updateSummaryOnFeedback.ts
+++ src/server/services/summary/updateSummaryOnFeedback.ts
@@ -8,14 +8,16 @@ export const updateSummaryAfterFeedbackCreated = async (feedbackTargetId: number
     where: { feedbackTargetId },
   })
 
+  const acualStudentCount = await UserFeedbackTarget.count({
+    where: { feedbackTargetId, accessStatus: 'STUDENT' },
+  })
+
   if (!summary) {
-    const studentCount = await UserFeedbackTarget.count({
-      where: { feedbackTargetId, accessStatus: 'STUDENT' },
-    })
-    summary = await createSummaryForFeedbackTarget(feedbackTargetId, studentCount, new Date(), new Date())
+    summary = await createSummaryForFeedbackTarget(feedbackTargetId, acualStudentCount, new Date(), new Date())
   }
 
   summary.data = addFeedbackDataToSummary(summary.data, feedback.data)
+  summary.data.studentCount = acualStudentCount
 
   const cachedFbt = await feedbackTargetCache.get(feedbackTargetId)
   if (cachedFbt) {
