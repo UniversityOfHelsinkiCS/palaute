bfa75f73f22eac709f3535f156aa2de7f7b55acf
Veikkosuhonen <vesuvesuh@gmail.com>
2025-04-01 13:45:32 +0300

    ci: Improve dot only script

--- scripts/grep_dot_only.sh
+++ scripts/grep_dot_only.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #Original script from https://github.com/UniversityOfHelsinkiCS/mobvita
 
-if git grep -qE --cached "(it|describe)\.only" $(git diff --cached --name-only) ./cypress; then
-    echo "Do not commit .only-tests. Did you remember to git add?"
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+FILES_WITH_ONLY=$(git grep -lE --cached "(it|describe)\.only" $(git diff --cached --name-only) ./cypress)
+
+if [[ -n "$FILES_WITH_ONLY" ]]; then
+    echo -e "${RED}Do not commit .only-tests.${NC} Found in the following files:"
+    echo -e "${YELLOW}$FILES_WITH_ONLY${NC}"
     exit 1
 else
     # echo "No .only-tests found"
