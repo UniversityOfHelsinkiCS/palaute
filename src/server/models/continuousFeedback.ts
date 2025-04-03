337b047aae8f5a85f5e1d1ff948ada91587fcac9
Veikkosuhonen <vesuvesuh@gmail.com>
2025-04-03 14:03:42 +0300

    refactor: Täsäfy continuousFeedback

---
+++ src/server/models/continuousFeedback.ts
@@ -0,0 +1,79 @@
+import {
+  Model,
+  INTEGER,
+  STRING,
+  JSONB,
+  BOOLEAN,
+  TEXT,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DATE,
+} from 'sequelize'
+import { sequelize } from '../db/dbConnection'
+
+class ContinuousFeedback extends Model<
+  InferAttributes<ContinuousFeedback>,
+  InferCreationAttributes<ContinuousFeedback>
+> {
+  // --- Acual DB columns ---
+  // ------------------------
+  declare id: CreationOptional<number>
+  declare data: Record<string, any>
+  declare userId: string
+  declare feedbackTargetId: number
+  declare sendInDigestEmail: CreationOptional<boolean>
+  declare response: CreationOptional<string>
+  declare responseEmailSent: CreationOptional<boolean>
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
+}
+
+ContinuousFeedback.init(
+  {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    data: {
+      type: JSONB,
+      allowNull: false,
+    },
+    userId: {
+      type: STRING,
+      allowNull: false,
+    },
+    feedbackTargetId: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    sendInDigestEmail: {
+      type: BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    response: {
+      type: TEXT,
+    },
+    responseEmailSent: {
+      type: BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    createdAt: {
+      type: DATE,
+    },
+    updatedAt: {
+      type: DATE,
+    },
+  },
+  {
+    underscored: true,
+    timestamps: true,
+    sequelize,
+  }
+)
+
+export { ContinuousFeedback }
