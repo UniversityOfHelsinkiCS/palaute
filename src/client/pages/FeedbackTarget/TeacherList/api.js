3eb9f0f806c3724e9e2dbd1b2e0706d8794640bd
Sini Arkko <sini.arkko@gmail.com>
2025-08-04 10:19:17 +0300

    fix: Invalidate feedbackTarget query when removing an administrative person

--- src/client/pages/FeedbackTarget/TeacherList/api.js
+++ src/client/pages/FeedbackTarget/TeacherList/api.js
@@ -1,5 +1,7 @@
 import apiClient from '../../../util/apiClient'
+import queryClient from '../../../util/queryClient'
 
 export const deleteResponsibleTeacher = async (feedbackTarget, teacher) => {
   await apiClient.delete(`/feedback-targets/${feedbackTarget.id}/user-feedback-targets/${teacher.id}`)
+  queryClient.invalidateQueries(['feedbackTarget', String(feedbackTarget.id)])
 }
