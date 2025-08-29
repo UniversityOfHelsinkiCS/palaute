e111b66e32e39caebc051a1163616c47c6bb0c39
Sini Arkko <sini.arkko@gmail.com>
2025-08-29 11:55:04 +0300

    refactor: Split exportXLSX function into smaller units

--- src/server/services/summary/exportXLSX.js
+++ src/server/services/summary/exportXLSX.js
@@ -19,35 +19,7 @@ const { getTeacherSummary } = require('./getTeacherSummary')
 const { getAccessibleCourseRealisationIds } = require('./access')
 const { ApplicationError } = require('../../util/customErrors')
 
-const exportXLSX = async ({
-  user,
-  startDate,
-  endDate,
-  includeOrgs,
-  includeCUs,
-  includeCURs,
-  allTime,
-  organisationId,
-}) => {
-  const workbook = XLSX.utils.book_new()
-
-  const scopedSummary = getScopedSummary({ startDate, endDate, allTime })
-
-  const organisationCode = organisationId ? await getOrganisationCodeById(organisationId) : undefined
-
-  const questions = await getSummaryQuestions(organisationCode)
-  const accessibleOrganisationIds = await getSummaryAccessibleOrganisationIds(user)
-
-  if (organisationId && !accessibleOrganisationIds.includes(organisationId)) {
-    return ApplicationError.Forbidden('User does not have access to the organisation')
-  }
-  const organisationIds = organisationId ? [organisationId] : accessibleOrganisationIds
-
-  let earliestStartDate = new Date(startDate)
-  let latestEndDate = new Date(endDate)
-
-  const teacherOrganisations = await getTeacherSummary({ user, startDate, endDate, allTime })
-
+const getOrganisations = async (scopedSummary, organisationIds) => {
   const organisations = await Organisation.findAll({
     attributes: ['id', 'name', 'code'],
     where: {
@@ -78,8 +50,10 @@ const exportXLSX = async ({
     order: [['code', 'ASC']],
   })
 
-  const courseUnitIds = organisations.flatMap(org => org.courseUnitsOrganisations.map(cuo => cuo.courseUnitId))
+  return organisations
+}
 
+const getCourseUnits = async (scopedSummary, courseUnitIds) => {
   const courseUnits = await CourseUnit.findAll({
     where: {
       id: courseUnitIds,
@@ -100,174 +74,231 @@ const exportXLSX = async ({
     ],
   })
 
-  const t = i18n.getFixedT(user.language)
+  return courseUnits
+}
+
+const getCourseRealisations = async (scopedSummary, courseRealisationIds) => {
+  const courseRealisations = await CourseRealisation.findAll({
+    attributes: ['id', 'name', 'startDate', 'endDate'],
+    where: {
+      id: _.uniq(courseRealisationIds),
+    },
+    include: [
+      {
+        model: scopedSummary,
+        as: 'summary',
+        required: true,
+      },
+      {
+        model: CourseRealisationsOrganisation,
+        as: 'courseRealisationsOrganisations',
+        attributes: ['organisationId'],
+        required: true,
+      },
+      {
+        model: FeedbackTarget,
+        as: 'feedbackTargets',
+        attributes: ['id'],
+        required: true,
+        include: {
+          model: CourseUnit,
+          as: 'courseUnit',
+          attributes: ['courseCode'],
+          required: true,
+        },
+      },
+    ],
+  })
+
+  return courseRealisations
+}
+
+const getAccessibleCourseRealisations = async (user, courseRealisations) => {
+  const orgAccess = await user.getOrganisationAccess()
+  const accessibleCurIds = await getAccessibleCourseRealisationIds(user)
+
+  const accessibleCourseRealisations = courseRealisations.filter(cur => {
+    const curOrgIds = cur.courseRealisationsOrganisations?.map(curo => curo.organisationId) ?? []
+    const hasOrgAccess = Object.values(orgAccess).some(o => curOrgIds.includes(o.organisation.id))
+    if (user.isAdmin || hasOrgAccess) return true
+
+    return accessibleCurIds.includes(cur.id)
+  })
+
+  return accessibleCourseRealisations
+}
+
+const getDefaultHeaders = (questions, t, language) => {
   const defaultHeaders = questions
-    .map(q => getLanguageValue(q.data.label, user.language))
+    .map(q => getLanguageValue(q.data?.label, language))
     .concat([t('common:studentCount'), t('courseSummary:feedbackCount'), t('courseSummary:feedbackResponsePercentage')])
 
-  if (includeOrgs) {
-    const organisationsJson = organisations.map(org => {
-      org.summary = sumSummaries(org.summaries)
-      delete org.dataValues.summaries
-      earliestStartDate = org.summary.startDate
-      latestEndDate = org.summary.endDate
-      return org.toJSON()
-    })
+  return defaultHeaders
+}
 
-    const allOrganisations = _.uniqBy(
-      organisationId ? organisationsJson : organisationsJson.concat(teacherOrganisations),
+// In xlsx terms aoa = array of arrays
+const commonPartOfAoa = (language, questions, target) => {
+  const targetName = getLanguageValue(target.name, language)
+  const summaryData = target.summary.data
+
+  const questionResults = questions.map(q => summaryData.result[q.id]?.mean ?? 0)
+
+  return [
+    targetName,
+    ...questionResults,
+    summaryData.studentCount,
+    summaryData.feedbackCount,
+    summaryData.feedbackResponsePercentage * 100,
+  ]
+}
+
+const getUniqueHeaders = (t, sheetName) => {
+  if (sheetName === 'organisations') return [t('common:organisationCode'), t('common:name')]
+
+  if (sheetName === 'course-units') return ['group_id', t('common:courseCode'), t('common:name')]
+
+  return ['id', t('common:courseCode'), t('common:name'), t('common:startDate'), t('common:endDate')]
+}
+
+const getJSON = (targets, targetType) => {
+  const targetJSON = targets.map(target => {
+    target.summary = sumSummaries(targetType === 'org' ? target.summaries : target.groupSummaries)
+    if (targetType === 'org') {
+      delete target.dataValues.summaries
+    } else {
+      delete target.dataValues.groupSummaries
+    }
+
+    return target.toJSON()
+  })
+
+  return targetJSON
+}
+
+const mapCourseIdsToCourseCodes = (teacherOrganisations, courseRealisations) => {
+  const curIdToCourseCodeMapping = {}
+  const teacherOrgCUs = teacherOrganisations.flatMap(org => org.courseUnits)
+
+  teacherOrgCUs.forEach(cu => {
+    cu.courseRealisations.forEach(cur => {
+      curIdToCourseCodeMapping[cur.id] = cu.courseCode
+    })
+  })
+
+  courseRealisations.forEach(cur => {
+    cur.feedbackTargets.forEach(fbt => {
+      curIdToCourseCodeMapping[cur.id] = fbt.courseUnit.courseCode
+    })
+  })
+
+  return curIdToCourseCodeMapping
+}
+
+const getOrganisationCourseRealisationIds = (organisations, courseUnits) =>
+  organisations
+    .flatMap(org => org.courseRealisationsOrganisations.map(curo => curo.courseRealisationId))
+    .concat(courseUnits.flatMap(cu => cu.feedbackTargets.map(fbt => fbt.courseRealisationId)))
+
+const exportXLSX = async ({
+  user,
+  startDate,
+  endDate,
+  includeOrgs,
+  includeCUs,
+  includeCURs,
+  allTime,
+  organisationId,
+}) => {
+  const accessibleOrganisationIds = await getSummaryAccessibleOrganisationIds(user)
+
+  if (organisationId && !accessibleOrganisationIds.includes(organisationId)) {
+    return ApplicationError.Forbidden('User does not have access to the organisation')
+  }
+
+  const t = i18n.getFixedT(user.language)
+
+  const organisationCode = organisationId ? await getOrganisationCodeById(organisationId) : undefined
+  const questions = await getSummaryQuestions(organisationCode)
+  const defaultHeaders = getDefaultHeaders(questions, t, user.language)
+
+  const workbook = XLSX.utils.book_new()
+  const addSheetToWorkbook = (dataAoa, sheetName) => {
+    const headers = [[...getUniqueHeaders(t, sheetName), ...defaultHeaders]]
+    XLSX.utils.book_append_sheet(workbook, XLSX.utils.aoa_to_sheet(headers.concat(dataAoa)), sheetName)
+  }
+
+  const scopedSummary = getScopedSummary({ startDate, endDate, allTime })
+
+  const teacherOrganisations = await getTeacherSummary({ user, startDate, endDate, allTime })
+
+  const organisationIds = organisationId ? [organisationId] : accessibleOrganisationIds
+  const organisations = await getOrganisations(scopedSummary, organisationIds)
+
+  const courseUnitIds = organisations.flatMap(org => org.courseUnitsOrganisations.map(cuo => cuo.courseUnitId))
+  const courseUnits = await getCourseUnits(scopedSummary, courseUnitIds)
+
+  const getAllTargets = (organisationTargets, targetType) => {
+    let teacherOrganisationsTargets = teacherOrganisations
+
+    if (targetType === 'cu') {
+      teacherOrganisationsTargets = teacherOrganisations.flatMap(org => org.courseUnits)
+    } else if (targetType === 'cur') {
+      teacherOrganisationsTargets = teacherOrganisations.flatMap(org =>
+        org.courseUnits.flatMap(cu => cu.courseRealisations)
+      )
+    }
+
+    const allTargets = _.uniqBy(
+      organisationId ? organisationTargets : organisationTargets.concat(teacherOrganisationsTargets),
       'id'
     )
 
-    // In xlsx terms aoa = array of arrays
-    const organisationsAoa = allOrganisations.map(org => [
-      org.code,
-      getLanguageValue(org.name, user.language),
-      ...questions.map(q => org.summary.data.result[q.id]?.mean ?? 0),
-      org.summary.data.studentCount,
-      org.summary.data.feedbackCount,
-      org.summary.data.feedbackResponsePercentage * 100,
-    ])
+    return allTargets
+  }
 
-    const headers = [[t('common:organisationCode'), t('common:name'), ...defaultHeaders]]
+  if (includeOrgs) {
+    const allOrganisations = getAllTargets(getJSON(organisations, 'org'), 'org')
 
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.aoa_to_sheet(headers.concat(organisationsAoa)), 'organisations')
+    const organisationsAoa = allOrganisations.map(org => [org.code, ...commonPartOfAoa(user.language, questions, org)])
+
+    addSheetToWorkbook(organisationsAoa, 'organisations')
   }
 
   if (includeCUs) {
-    const courseUnitsJson = _.uniqBy(courseUnits, 'groupId').map(cu => {
-      cu.summary = sumSummaries(cu.groupSummaries)
-      delete cu.dataValues.summaries
-      earliestStartDate = cu.summary.startDate
-      latestEndDate = cu.summary.endDate
-      return cu.toJSON()
-    })
-
-    const allCourseUnits = _.uniqBy(
-      organisationId ? courseUnitsJson : courseUnitsJson.concat(teacherOrganisations.flatMap(org => org.courseUnits)),
-      'id'
-    )
+    const allCourseUnits = getAllTargets(getJSON(_.uniqBy(courseUnits, 'groupId'), 'cu'), 'cu')
 
     const courseUnitsAoa = allCourseUnits.map(cu => [
       cu.groupId,
       cu.courseCode,
-      getLanguageValue(cu.name, user.language),
-      ...questions.map(q => cu.summary.data.result[q.id]?.mean ?? 0),
-      cu.summary.data.studentCount,
-      cu.summary.data.feedbackCount,
-      cu.summary.data.feedbackResponsePercentage * 100,
+      ...commonPartOfAoa(user.language, questions, cu),
     ])
 
-    const headers = [['group_id', t('common:courseCode'), t('common:name'), ...defaultHeaders]]
-
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.aoa_to_sheet(headers.concat(courseUnitsAoa)), 'course-units')
+    addSheetToWorkbook(courseUnitsAoa, 'course-units')
   }
 
   if (includeCURs) {
-    const organisationCourseRealisationIds = organisations
-      .flatMap(org => org.courseRealisationsOrganisations.map(curo => curo.courseRealisationId))
-      .concat(courseUnits.flatMap(cu => cu.feedbackTargets.map(fbt => fbt.courseRealisationId)))
+    const organisationCourseRealisationIds = getOrganisationCourseRealisationIds(organisations, courseUnits)
 
-    const orgAccess = await user.getOrganisationAccess()
-    const accessibleCurIds = await getAccessibleCourseRealisationIds(user)
+    const courseRealisations = await getCourseRealisations(scopedSummary, organisationCourseRealisationIds)
 
-    const courseRealisations = await CourseRealisation.findAll({
-      attributes: ['id', 'name', 'startDate', 'endDate'],
-      where: {
-        id: _.uniq(organisationCourseRealisationIds),
-      },
-      include: [
-        {
-          model: scopedSummary,
-          as: 'summary',
-          required: true,
-        },
-        {
-          model: CourseRealisationsOrganisation,
-          as: 'courseRealisationsOrganisations',
-          attributes: ['organisationId'],
-          required: true,
-        },
-        {
-          model: FeedbackTarget,
-          as: 'feedbackTargets',
-          attributes: ['id'],
-          required: true,
-          include: {
-            model: CourseUnit,
-            as: 'courseUnit',
-            attributes: ['courseCode'],
-            required: true,
-          },
-        },
-      ],
-    })
+    const allCourseRealisations = getAllTargets(courseRealisations, 'cur')
 
-    const curIdToCourseCode = {}
+    const accessibleCourseRealisations = await getAccessibleCourseRealisations(user, allCourseRealisations)
 
-    const teacherOrgCUs = teacherOrganisations.flatMap(org => org.courseUnits)
-
-    teacherOrgCUs.forEach(cu => {
-      cu.courseRealisations.forEach(cur => {
-        curIdToCourseCode[cur.id] = cu.courseCode
-      })
-    })
-
-    courseRealisations.forEach(cur => {
-      cur.feedbackTargets.forEach(fbt => {
-        curIdToCourseCode[cur.id] = fbt.courseUnit.courseCode
-      })
-    })
-
-    const allCourseRealisations = _.uniqBy(
-      organisationId
-        ? courseRealisations
-        : courseRealisations.concat(
-            teacherOrganisations.flatMap(org => org.courseUnits.flatMap(cu => cu.courseRealisations))
-          ),
-      'id'
-    )
-
-    const accessibleCourseRealisations = allCourseRealisations.filter(cur => {
-      const curOrgIds = cur.courseRealisationsOrganisations?.map(curo => curo.organisationId) ?? []
-      const hasOrgAccess = Object.values(orgAccess).some(o => curOrgIds.includes(o.organisation.id))
-      if (user.isAdmin || hasOrgAccess) return true
-
-      return accessibleCurIds.includes(cur.id)
-    })
+    const curIdToCourseCode = mapCourseIdsToCourseCodes(teacherOrganisations, courseRealisations)
 
     const courseRealisationsAoa = accessibleCourseRealisations.map(cur => {
-      earliestStartDate = cur.summary.startDate
-      latestEndDate = cur.summary.endDate
       const courseCode = curIdToCourseCode[cur.id] || ''
+      const [curName, ...rest] = commonPartOfAoa(user.language, questions, cur)
 
-      return [
-        cur.id,
-        courseCode,
-        getLanguageValue(cur.name, user.language),
-        cur.startDate,
-        cur.endDate,
-        ...questions.map(q => cur.summary.data.result[q.id]?.mean ?? 0),
-        cur.summary.data.studentCount,
-        cur.summary.data.feedbackCount,
-        cur.summary.data.feedbackResponsePercentage * 100,
-      ]
+      return [cur.id, courseCode, curName, cur.startDate, cur.endDate, ...rest]
     })
 
-    const headers = [
-      ['id', t('common:courseCode'), t('common:name'), t('common:startDate'), t('common:endDate'), ...defaultHeaders],
-    ]
-
-    XLSX.utils.book_append_sheet(
-      workbook,
-      XLSX.utils.aoa_to_sheet(headers.concat(courseRealisationsAoa)),
-      'course-realisations'
-    )
+    addSheetToWorkbook(courseRealisationsAoa, 'course-realisations')
   }
 
   const xlsxFile = XLSX.write(workbook, { type: 'buffer', bookSST: false })
-
-  const fileName = `norppa_${earliestStartDate}-${latestEndDate}.xlsx`
+  const fileName = `norppa_${startDate}-${endDate}.xlsx`
 
   return {
     xlsxFile,
