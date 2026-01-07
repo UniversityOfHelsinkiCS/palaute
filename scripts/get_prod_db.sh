a50a75dd687ee26cbb10431892a305dc9446921c
Merivirta Jasse J <mjasse@lx2-500-170.ad.helsinki.fi>
2026-01-07 12:52:26 +0200

    fix: update docker-compose to docker compose

--- scripts/get_prod_db.sh
+++ scripts/get_prod_db.sh
@@ -17,11 +17,10 @@ DOCKER_COMPOSE=$PROJECT_ROOT/docker-compose.yml
 
 S3_CONF=~/.s3cfg
 
-retry () {
-    for i in {1..60}
-    do
-        $@ && break || echo "Retry attempt $i failed, waiting..." && sleep 3;
-    done
+retry() {
+  for i in {1..60}; do
+    $@ && break || echo "Retry attempt $i failed, waiting..." && sleep 3
+  done
 }
 
 if [ ! -f "$S3_CONF" ]; then
@@ -75,7 +74,6 @@ select chosen_jami_backup in $jami_backup_files; do
   fi
 done
 
-
 # --- Downloads ---
 echo "Fetching the selected Norppa dump: $FILE_NAME"
 s3cmd -c "$S3_CONF" get "$chosen_backup" "$BACKUPS"
@@ -95,19 +93,20 @@ fi
 
 # --- Reset and Start ---
 echo "Removing database and related volume"
-docker-compose -f $DOCKER_COMPOSE down -v
+docker compose -f $DOCKER_COMPOSE down -v
 
 echo "Starting postgres containers in the background"
-docker-compose -f $DOCKER_COMPOSE up -d $SERVICE_NAME $JAMI_SERVICE_NAME
+docker compose -f $DOCKER_COMPOSE up -d $SERVICE_NAME $JAMI_SERVICE_NAME
 
 # --- Populate Norppa ---
-retry docker-compose -f $DOCKER_COMPOSE exec $SERVICE_NAME pg_isready --dbname=$DB_NAME
+retry docker compose -f $DOCKER_COMPOSE exec $SERVICE_NAME pg_isready --dbname=$DB_NAME
 
 echo "Populating ${FOLDER_NAME}"
-docker exec -i $CONTAINER /bin/bash -c "gunzip | psql -U postgres" < ${BACKUPS}${FILE_NAME}
+docker exec -i $CONTAINER /bin/bash -c "gunzip | psql -U postgres" <${BACKUPS}${FILE_NAME}
 
 # --- Populate Jami ---
-retry docker-compose -f $DOCKER_COMPOSE exec $JAMI_SERVICE_NAME pg_isready --dbname=$JAMI_DB_NAME
+retry docker compose -f $DOCKER_COMPOSE exec $JAMI_SERVICE_NAME pg_isready --dbname=$JAMI_DB_NAME
 
 echo "Populating ${JAMI_FOLDER_NAME}"
-docker exec -i $JAMI_CONTAINER /bin/bash -c "gunzip | psql -U postgres" < ${BACKUPS}${JAMI_FILE_NAME}
+docker exec -i $JAMI_CONTAINER /bin/bash -c "gunzip | psql -U postgres" <${BACKUPS}${JAMI_FILE_NAME}
+
