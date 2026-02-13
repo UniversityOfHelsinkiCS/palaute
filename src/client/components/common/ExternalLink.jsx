a727ad087eb4dc092d6b4b253b30cab18978c14a
Sini Arkko <sini.arkko@gmail.com>
2026-02-13 10:24:36 +0200

    Add visually hidden info ('opens in a new tab') to external links for screen readers

--- src/client/components/common/ExternalLink.jsx
+++ src/client/components/common/ExternalLink.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from '@mui/material'
 import ExternalLinkIcon from '@mui/icons-material/OpenInNew'
+import { visuallyHidden } from '@mui/utils'
 
 const styles = {
   icon: {
@@ -9,11 +11,16 @@ const styles = {
   },
 }
 
-const ExternalLink = ({ children, ...props }) => (
-  <Link target="_blank" rel="noopener" {...props} underline="always">
-    {children}
-    <ExternalLinkIcon sx={styles.icon} />
-  </Link>
-)
+const ExternalLink = ({ children, ...props }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Link target="_blank" rel="noopener" {...props} underline="always">
+      {children}
+      <ExternalLinkIcon sx={styles.icon} aria-hidden="true" />
+      <span style={visuallyHidden}>{t('common:opensInNewTab')}</span>
+    </Link>
+  )
+}
 
 export default ExternalLink
--- src/client/components/common/LinkButton.jsx
+++ src/client/components/common/LinkButton.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { Button, Link as MuiLink } from '@mui/material'
 import { OpenInNew } from '@mui/icons-material'
+import { visuallyHidden } from '@mui/utils'
 
 const LinkButton = ({ title, to, external = false, ...rest }) => {
+  const { t } = useTranslation()
   const buttonProps = {
     ...(external
-      ? { component: MuiLink, href: to, rel: 'noopener noreferrer', target: '_blank', endIcon: <OpenInNew /> }
+      ? {
+          component: MuiLink,
+          href: to,
+          rel: 'noopener noreferrer',
+          target: '_blank',
+          endIcon: <OpenInNew aria-hidden="true" />,
+        }
       : { component: Link, to }),
   }
 
@@ -18,6 +27,7 @@ const LinkButton = ({ title, to, external = false, ...rest }) => {
       sx={{ textDecoration: 'underline', '&:hover': { textDecoration: 'underline' } }}
     >
       {title}
+      {external && <span style={visuallyHidden}>{t('common:opensInNewTab')}</span>}
     </Button>
   )
 }
