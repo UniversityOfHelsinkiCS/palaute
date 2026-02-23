7f68454861bf33a49fab7f6c678f613d559d7f16
Sini Arkko <sini.arkko@gmail.com>
2026-02-23 12:06:01 +0200

    fix: Second attempt to fix import

---
+++ src/client/util/accessibility.jsx
@@ -0,0 +1,7 @@
+export const focusIndicatorStyle = {
+  '&.Mui-focusVisible': {
+    outline: '3px solid',
+    outlineColor: theme => theme.palette.primary.main,
+    outlineOffset: '3px',
+  },
+}
