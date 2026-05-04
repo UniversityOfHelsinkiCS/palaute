955fac2089e5d60ff7ee38ae045c10850ab24ad7
Sini Arkko <sini.arkko@gmail.com>
2026-05-04 13:04:52 +0300

    refactor: Remove commented code

--- src/client/pages/FeedbackTarget/tabs/Results/QuestionResults/ChartAccessibilityWrapper.jsx
+++ src/client/pages/FeedbackTarget/tabs/Results/QuestionResults/ChartAccessibilityWrapper.jsx
@@ -2,19 +2,6 @@ import React from 'react'
 import { Box } from '@mui/material'
 
 const ChartAccessibilityWrapper = ({ chart, table, showTable }) => (
-  // useEffect(() => {
-  //   const handleKeyDown = event => {
-  //     // Alt + T to toggle between chart and table
-  //     if (event.altKey && event.key === 't') {
-  //       event.preventDefault()
-  //       setShowTable(prev => !prev)
-  //     }
-  //   }
-
-  //   window.addEventListener('keydown', handleKeyDown)
-  //   return () => window.removeEventListener('keydown', handleKeyDown)
-  // }, [])
-
   <>
     {!showTable && (
       <Box role="img" aria-label="Chart visualization of results">
