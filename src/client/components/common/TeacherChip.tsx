a9f91454c9c7cb0cbee5d6f03cfeb238204f3197
Sini Arkko <sini.arkko@gmail.com>
2026-06-15 17:34:10 +0300

    fix: Add aria-label and aria-description to TeacherChip

--- src/client/components/common/TeacherChip.tsx
+++ src/client/components/common/TeacherChip.tsx
@@ -1,5 +1,6 @@
 import { Chip, Tooltip } from '@mui/material'
 import React, { CSSProperties } from 'react'
+import { useTranslation } from 'react-i18next'
 import type { User } from '@common/types/user'
 import { focusIndicatorStyle } from '../../util/accessibility'
 
@@ -12,25 +13,31 @@ interface TeacherChipProps {
   tooltipPlacement: 'bottom' | 'top' | 'left' | 'right'
 }
 
-const TeacherChip = ({ user, onDelete, tooltip, outlined, style, tooltipPlacement }: TeacherChipProps) => (
-  <Tooltip title={tooltip || user.email} placement={tooltipPlacement}>
-    <Chip
-      variant={outlined ? 'outlined' : 'filled'}
-      label={`${user.firstName ?? ''} ${user.lastName ?? ''}`}
-      onDelete={
-        onDelete &&
-        (e => {
-          e?.preventDefault()
-          onDelete()
-        })
-      }
-      clickable={user.email !== null}
-      component="a"
-      href={user.email ? `mailto:${user.email}` : undefined}
-      size="small"
-      sx={{ margin: '1px', ...style, fontWeight: 'normal', ...focusIndicatorStyle() }}
-    />
-  </Tooltip>
-)
+const TeacherChip = ({ user, onDelete, tooltip, outlined, style, tooltipPlacement }: TeacherChipProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <Tooltip title={tooltip || user.email} placement={tooltipPlacement}>
+      <Chip
+        variant={outlined ? 'outlined' : 'filled'}
+        label={`${user.firstName ?? ''} ${user.lastName ?? ''}`.trim()}
+        aria-label={`${tooltip ?? ''} ${user.firstName ?? ''} ${user.lastName ?? ''}`.trim()}
+        aria-description={user.email !== null ? t('common:contactEmailDescription', { email: user.email }) : undefined}
+        onDelete={
+          onDelete &&
+          (e => {
+            e?.preventDefault()
+            onDelete()
+          })
+        }
+        clickable={user.email !== null}
+        component="a"
+        href={user.email ? `mailto:${user.email}` : undefined}
+        size="small"
+        sx={{ margin: '1px', ...style, fontWeight: 'normal', ...focusIndicatorStyle() }}
+      />
+    </Tooltip>
+  )
+}
 
 export default TeacherChip
