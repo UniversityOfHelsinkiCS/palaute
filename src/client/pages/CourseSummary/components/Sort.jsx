6cb4cda4f663f519d328b4e967a9d55865782acc
Sini Arkko <sini.arkko@gmail.com>
2026-09-03 11:46:48 +0300

    fix: Fix sort button labeling and add sort status to tooltip

--- src/client/pages/CourseSummary/components/Sort.jsx
+++ src/client/pages/CourseSummary/components/Sort.jsx
@@ -5,9 +5,10 @@ import { useTranslation } from 'react-i18next'
 import { focusIndicatorStyle } from '../../../util/accessibility'
 import { useSummaryContext } from '../context'
 
-const AngledHeading = ({ label, isActive }) => (
+const AngledHeading = ({ id, label, isActive }) => (
   <Tooltip title={label} placement="bottom">
     <Typography
+      id={id}
       color={isActive ? 'text.primary' : 'text.secondary'}
       sx={{
         position: 'absolute',
@@ -36,6 +37,12 @@ const Sort = ({ field, label, width }) => {
   const isActive = currentSortByField === field
   const isNextDesc = !isActive || !isDesc
 
+  const labelId = `sort-${field}-label`
+
+  const sortAction = `${t('common:sort')} ${isNextDesc ? t('common:descending') : t('common:ascending')}`
+  const sortStatus = isDesc ? t('common:sortedDescending') : t('common:sortedAscending')
+  const tooltipTitle = isActive ? `${sortStatus}. ${sortAction}.` : sortAction
+
   return (
     <Box sx={{ display: 'flex', width, justifyContent: 'center', flexShrink: 0 }}>
       <Box
@@ -47,27 +54,23 @@ const Sort = ({ field, label, width }) => {
           pt: '10rem',
         }}
       >
-        <AngledHeading label={label} isActive={isActive} />
-        <Tooltip
-          title={`${t('common:sort')} ${isNextDesc ? t('common:descending') : t('common:ascending')}`}
-          placement="bottom"
-        >
-          <div>
-            <IconButton
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                p: '1rem',
-                ...focusIndicatorStyle(),
-              }}
-              onClick={() => setSortBy([field, isNextDesc ? 'desc' : 'asc'])}
-              color="primary"
-              disableRipple
-            >
-              <ArrowDropUp color={isActive && isDesc ? 'primary' : 'disabled'} sx={{ m: '-0.5rem' }} />
-              <ArrowDropDown color={isActive && isAsc ? 'primary' : 'disabled'} sx={{ m: '-0.5rem' }} />
-            </IconButton>
-          </div>
+        <AngledHeading id={labelId} label={label} isActive={isActive} />
+        <Tooltip title={tooltipTitle} placement="bottom">
+          <IconButton
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              p: '1rem',
+              ...focusIndicatorStyle(),
+            }}
+            onClick={() => setSortBy([field, isNextDesc ? 'desc' : 'asc'])}
+            color="primary"
+            disableRipple
+            aria-describedby={labelId}
+          >
+            <ArrowDropUp color={isActive && isDesc ? 'primary' : 'disabled'} sx={{ m: '-0.5rem' }} />
+            <ArrowDropDown color={isActive && isAsc ? 'primary' : 'disabled'} sx={{ m: '-0.5rem' }} />
+          </IconButton>
         </Tooltip>
       </Box>
     </Box>
